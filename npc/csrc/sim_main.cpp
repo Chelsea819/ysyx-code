90e3d0ad31e7706a5c9a139b3041f454e298bda7
tracer-ysyx <tracer@ysyx.org>
2023-07-16 11:38:15 +0800

    > sim RTL ysyx_22041211 王晓雪 Linux chelsea-virtual-machine 5.19.0-35-generic #36~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Feb 17 15:17:25 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  11:38:15 up  4:13,  3 users,  load average: 0.34, 0.24, 0.20

--- npc/csrc/sim_main.cpp
+++ npc/csrc/sim_main.cpp
@@ -24,31 +24,31 @@ void nvboard_bind_all_pins(Vtop* top);
 
 int main(int argc, char** argv, char** env) {
 
-	Vtop *dut = new Vtop;
+//	Vtop *dut = new Vtop;
 
 
-	nvboard_bind_all_pins(dut);
+	nvboard_bind_all_pins(&dut);
 	nvboard_init();
 
 	Verilated::traceEverOn(true);
 	VerilatedVcdC *m_trace = new VerilatedVcdC;  
-	dut->trace(m_trace, 5);               
+	dut.trace(m_trace, 5);               
 	m_trace->open("waveform.vcd");
 	while (sim_time < MAX_SIM_TIME) {
 		int a1 = rand() & 1;
 		int b1 = rand() & 1;
 		//	dut->clk ^= 1; 
-		dut->a = a1;
-		dut->b = b1;
-		dut->eval();
+		dut.a = a1;
+		dut.b = b1;
+		dut.eval();
 		printf("a = %d, b = %d", a1, b1);
 		m_trace->dump(sim_time);
 		sim_time++;
-		assert(dut->f == (a1 ^ b1));
+		assert(dut.f == (a1 ^ b1));
 		nvboard_update();
 	}
 	m_trace->close();
-	delete dut;
+	//delete dut;
 	exit(EXIT_SUCCESS);
 }
 
