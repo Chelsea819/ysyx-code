2b719e94e1d3b4c659e5c99abbf8a25dc90db8e8
tracer-ysyx <tracer@ysyx.org>
2023-07-25 21:09:26 +0800

    > compile NEMU ysyx_22041211 王晓雪 Linux chelsea-virtual-machine 5.19.0-35-generic #36~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Feb 17 15:17:25 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:09:26 up 5 days,  2:08,  5 users,  load average: 0.94, 0.75, 0.57

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -91,9 +91,9 @@ static int cmd_help(char *args);
 static int cmd_pcount(char *args){
   //unsigned int addr = convert_16(args);
   //printf("result = 0x%08x\n",addr);
-  bool *success = NULL;
-  if(args) *success = true;
-  expr(args,success);
+  bool success = false;
+  if(args) success = true;
+  expr(args,&success);
   return 0;
 }
 
