265eb9a74243e19698721ac949af34549fb49774
tracer-ysyx <tracer@ysyx.org>
2023-07-25 20:57:51 +0800

    > compile NEMU ysyx_22041211 王晓雪 Linux chelsea-virtual-machine 5.19.0-35-generic #36~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Feb 17 15:17:25 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:57:51 up 5 days,  1:57,  5 users,  load average: 0.32, 0.41, 0.36

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -49,7 +49,7 @@ static struct rule {
   {"/",TK_DIV},         // division
   {"(",TK_LEFT_BRA},    // bracket-left
   {")",TK_RIGHT_BRA},   // bracket-right
-  {"[0-9]+",TK_NUM},    // num
+  //{"[0-9]+",TK_NUM},    // num
 };
 
 #define NR_REGEX ARRLEN(rules)
