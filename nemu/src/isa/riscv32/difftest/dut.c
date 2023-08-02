c0fbe975e64d652dc368b956cbdd608970c3341c
tracer-ysyx <tracer@ysyx.org>
2023-08-02 21:12:29 +0800

    > compile NEMU ysyx_22041211 王晓雪 Linux chelsea-virtual-machine 5.19.0-35-generic #36~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Feb 17 15:17:25 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:12:29 up 17:50,  2 users,  load average: 1.12, 0.89, 0.57

--- nemu/src/isa/riscv32/difftest/dut.c
+++ nemu/src/isa/riscv32/difftest/dut.c
@@ -19,7 +19,7 @@
 
 bool isa_difftest_checkregs(CPU_state *ref_r, vaddr_t pc) {
   //printf("pc = 0x%x ref_r->pc = 0x%x\n",pc ,ref_r->pc);
-  //if(pc != ref_r->pc) return false;
+  if(pc != ref_r->pc) return false;
   for(int i = 0; i < 32; i++){
     if(ref_r->gpr[i] != gpr(i)) return false;
   } 
