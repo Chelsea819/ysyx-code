566f37b1ab99c05fb3e545961f90eed819f8c645
tracer-ysyx <tracer@ysyx.org>
2023-08-11 18:09:47 +0800

    > compile NEMU ysyx_22041211 王晓雪 Linux chelsea-virtual-machine 5.19.0-35-generic #36~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Feb 17 15:17:25 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  18:09:47 up 4 days, 18:18, 10 users,  load average: 1.66, 0.97, 0.64

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -25,12 +25,14 @@ int sprintf(char *out, const char *fmt, ...) {
   va_list ap;
   va_start(ap,fmt);
   int flag = 1;
+  int flag1 = 0;
   int tmp = 0;
   int num = 0;
   int k = 0;
   for(int i = 0; *(fmt + i) != '\0'; i++,k++){
-    if(fmt[i] == '%') {flag^=1; tmp = i;}
-    if(flag == 1 && i == tmp + 1){
+    if(fmt[i] == '%') {flag1^=1; tmp = i;}
+    if(flag1 == 1 && i == tmp + 1){
+        
       if(fmt[i] == 'd'){
         memset(arr_tmp,0,20 * sizeof(char));
         numAdd = 19;
@@ -39,23 +41,26 @@ int sprintf(char *out, const char *fmt, ...) {
           convert(num/flag);
           flag*=10;
         }
-        for(k = k - 1;numAdd < 20 ; k++){
-          out[k] = arr_tmp[++ numAdd];
+        k --;
+        numAdd ++;
+        for( ;numAdd < 20 ; k++,numAdd ++){
+          out[k] = arr_tmp[numAdd];
         }
+        k--;
       } 
       if(fmt[i] == 's'){
         char *str = va_arg(ap,char*);
         k --;
-        for(int j = 0; str[j] != '\0'; j++){
+        for(int j = 0; str[j] != '\0'; j++,k++){
           out[k] = str[j];
         }
+        k --;
       }
-      flag = 0;
+      flag1 = 0;
     }
     else  out[k] = fmt[i];
   }
   return k;
-  //panic("Not implemented");
 }
 
 int snprintf(char *out, size_t n, const char *fmt, ...) {
