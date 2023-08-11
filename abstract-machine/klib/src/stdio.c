e575a1489154a5489a004533c87c87504d5b8c41
tracer-ysyx <tracer@ysyx.org>
2023-08-11 16:09:39 +0800

    > compile NEMU ysyx_22041211 王晓雪 Linux chelsea-virtual-machine 5.19.0-35-generic #36~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Feb 17 15:17:25 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:09:38 up 4 days, 16:18,  9 users,  load average: 1.53, 0.96, 0.79

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -22,40 +22,40 @@ void convert(int num){
 }
 
 int sprintf(char *out, const char *fmt, ...) {
-  va_list ap;
-  va_start(ap,fmt);
-  int flag = 1;
-  int tmp = 0;
-  int num = 0;
-  int k = 0;
-  for(int i = 0; *(fmt + i) != '\0'; i++,k++){
-    if(fmt[i] == '%') {flag^=1; tmp = i;}
-    if(flag == 1 || i == tmp + 1){
-      if(fmt[i] == 'd'){
-        memset(arr_tmp,0,20 * sizeof(char));
-        numAdd = 19;
-        num = va_arg(ap,int);
-        while(num/flag != 0){
-          convert(num/flag);
-          flag*=10;
-        }
-        for(k = k - 1;numAdd < 20 ; k++){
-          out[k] = arr_tmp[++ numAdd];
-        }
-      } 
-      if(fmt[i] == 's'){
-        char *str = va_arg(ap,char*);
-        k --;
-        for(int j = 0; str[j] != '\0'; j++){
-          out[k] = str[j];
-        }
-      }
-      flag = 0;
-    }
-    else  out[k] = fmt[i];
-  }
-  return k;
-  //panic("Not implemented");
+  // va_list ap;
+  // va_start(ap,fmt);
+  // int flag = 1;
+  // int tmp = 0;
+  // int num = 0;
+  // int k = 0;
+  // for(int i = 0; *(fmt + i) != '\0'; i++,k++){
+  //   if(fmt[i] == '%') {flag^=1; tmp = i;}
+  //   if(flag == 1 || i == tmp + 1){
+  //     if(fmt[i] == 'd'){
+  //       memset(arr_tmp,0,20 * sizeof(char));
+  //       numAdd = 19;
+  //       num = va_arg(ap,int);
+  //       while(num/flag != 0){
+  //         convert(num/flag);
+  //         flag*=10;
+  //       }
+  //       for(k = k - 1;numAdd < 20 ; k++){
+  //         out[k] = arr_tmp[++ numAdd];
+  //       }
+  //     } 
+  //     if(fmt[i] == 's'){
+  //       char *str = va_arg(ap,char*);
+  //       k --;
+  //       for(int j = 0; str[j] != '\0'; j++){
+  //         out[k] = str[j];
+  //       }
+  //     }
+  //     flag = 0;
+  //   }
+  //   else  out[k] = fmt[i];
+  // }
+  // return k;
+  panic("Not implemented");
 }
 
 int snprintf(char *out, size_t n, const char *fmt, ...) {
