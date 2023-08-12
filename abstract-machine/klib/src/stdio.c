91de6ef861f0c638d93917b0e00e523c4002961b
tracer-ysyx <tracer@ysyx.org>
2023-08-12 11:54:31 +0800

    > compile NEMU ysyx_22041211 王晓雪 Linux chelsea-virtual-machine 5.19.0-35-generic #36~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Feb 17 15:17:25 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  11:54:31 up 5 days,  1:42, 10 users,  load average: 0.97, 0.56, 0.45

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -19,21 +19,77 @@ void convert(int num,int* numAdd,char *arr_tmp){
   arr_tmp[(*numAdd) --] = tmp + 48; 
 }
 
+// int sprintf(char *out, const char *fmt, ...) {
+//   char arr_tmp[20] = {0};  //存放数字转换成的字符
+//   int numAdd = 18;  //数组的下标
+
+//   va_list ap;
+//   va_start(ap,fmt);
+
+//   int flag = 1;    //位数
+//   int percent = 0; //检测%
+//   int tmp = 0;     //存放%的下标
+//   int num = 0;     //
+//   int k = 0;
+//   for(int i = 0; *(fmt + i) != '\0'; i++,k++){
+//     if(fmt[i] == '%') {percent^=1; tmp = i;}
+//     if(percent == 1 && i == tmp + 1){
+//       if(fmt[i] == 'd'){
+//         //memset_bk(arr_tmp,0,20 * sizeof(char));
+//         numAdd = 18;
+//         num = va_arg(ap,int);
+//         flag = 1;
+//         while(num/flag != 0){
+//           convert(num/flag,&numAdd,arr_tmp);
+//           flag*=10;
+//         }   
+//         k --;
+//         numAdd ++;
+//         for( ;numAdd < 19 ; k++,numAdd ++){
+//           out[k] = arr_tmp[numAdd];
+//         }
+//         k--;
+//       } 
+//       if(fmt[i] == 's'){
+//         char *str = va_arg(ap,char*);
+//         k --;
+//         for(int j = 0; str[j] != '\0'; j++,k++){
+//           out[k] = str[j];
+//         }
+//         k --;
+//       }
+//       percent = 0;
+//     }
+//     else  out[k] = fmt[i];
+//   }
+//   out[k + 1] = '\0';
+//   va_end(ap);
+//   return k + 1;
+// }
+
+
+//碰到%后，指示%的指针不变 pCurrent先前跑 找到格式化输出的格式
+
 int sprintf(char *out, const char *fmt, ...) {
-  char arr_tmp[20] = {0};
-  int numAdd = 18;
+  char arr_tmp[20] = {0};  //存放数字转换成的字符
+  int numAdd = 18;  //数组的下标
+
   va_list ap;
   va_start(ap,fmt);
-  int flag = 1;
+
+  int flag = 1;    //位数
   int percent = 0; //检测%
-  int tmp = 0; 
-  int num = 0; 
-  int k = 0;
+  int tmp = 0;     //存放%的下标
+  int num = 0;     //要替换的数字
+  int k = 0;       //out数组的下标
+
   for(int i = 0; *(fmt + i) != '\0'; i++,k++){
     if(fmt[i] == '%') {percent^=1; tmp = i;}
-    if(percent == 1 && i == tmp + 1){
+
+    //当percent为1时进入循环
+    else if(percent == 1 && i == tmp + 1){
+      // %d
       if(fmt[i] == 'd'){
-        //memset_bk(arr_tmp,0,20 * sizeof(char));
         numAdd = 18;
         num = va_arg(ap,int);
         flag = 1;
@@ -41,21 +97,18 @@ int sprintf(char *out, const char *fmt, ...) {
           convert(num/flag,&numAdd,arr_tmp);
           flag*=10;
         }   
-        k --;
         numAdd ++;
         for( ;numAdd < 19 ; k++,numAdd ++){
           out[k] = arr_tmp[numAdd];
         }
-        k--;
       } 
       if(fmt[i] == 's'){
         char *str = va_arg(ap,char*);
-        k --;
         for(int j = 0; str[j] != '\0'; j++,k++){
           out[k] = str[j];
         }
-        k --;
       }
+      k --;
       percent = 0;
     }
     else  out[k] = fmt[i];
