0c594ba84a9d7d61318f49ad3c93e8a5f626041a
tracer-ysyx <tracer@ysyx.org>
2023-08-14 21:26:40 +0800

    > compile NEMU ysyx_22041211 王晓雪 Linux chelsea-virtual-machine 5.19.0-35-generic #36~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Feb 17 15:17:25 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:26:40 up 6 days,  1:22,  9 users,  load average: 0.95, 1.33, 1.21

--- nemu/src/memory/paddr.c
+++ nemu/src/memory/paddr.c
@@ -29,7 +29,9 @@ uint8_t* guest_to_host(paddr_t paddr) { return pmem + paddr - CONFIG_MBASE; }
 paddr_t host_to_guest(uint8_t *haddr) { return haddr - pmem + CONFIG_MBASE; }
 
 static word_t pmem_read(paddr_t addr, int len) {
-  word_t ret = host_read(guest_to_host(addr), len);
+  uint8_t* host_addr = guest_to_host(addr);
+  word_t ret = host_read(host_addr, len);
+  Log("guest read: %08x",addr);
   return ret;
 }
 
