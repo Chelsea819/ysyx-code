ee84909cf2de35ac2386ea133b01193473ee093c
tracer-ysyx <tracer@ysyx.org>
2023-08-22 17:06:27 +0800

    > compile NEMU ysyx_22041211 王晓雪 Linux chelsea-virtual-machine 5.19.0-35-generic #36~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Feb 17 15:17:25 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:06:27 up 8 days, 16:29, 10 users,  load average: 0.88, 0.77, 0.54

--- nemu/src/monitor/monitor.c
+++ nemu/src/monitor/monitor.c
@@ -129,7 +129,7 @@ static int init_ftrace(){
     }
     printf("Elf_sec.sh_name = %d\n",Elf_sec.sh_name);
     if(Elf_sec.sh_type == SHT_SYMTAB){
-      printf("11111\n");
+      printf("Elf_sec.sh_name = %d\n",Elf_sec.sh_name);
       sym_off = Elf_sec.sh_offset;
       continue;
     }
