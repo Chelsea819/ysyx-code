8395022ad36347ea88e7ea232e5a8441d48a7318
tracer-ysyx <tracer@ysyx.org>
2023-09-08 17:26:31 +0800

    > sim RTL ysyx_22041211 王晓雪 Linux chelsea-virtual-machine 5.19.0-35-generic #36~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Feb 17 15:17:25 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:26:31 up 10:30,  2 users,  load average: 0.26, 0.09, 0.14

--- npc/csrc/sim_main.cpp
+++ npc/csrc/sim_main.cpp
@@ -55,17 +55,22 @@ void init_mem_npc(){
 	return ;
 }
 
-static inline uint32_t host_read(void *addr) { printf("before host_read\n");
-    return *(uint32_t *)addr;printf("after host_read\n");
+static inline uint32_t host_read(void *addr) { 
+	printf("before host_read\n");
+    return *(uint32_t *)addr;
+	printf("after host_read\n");
 }
 
 
 uint32_t* guest_to_host(uint32_t paddr) { 
 	printf("before  guest_to_host\n");
-	return pmem + (paddr - CONFIG_MBASE) / 4; printf("after guest_to_host\n");}
+	return pmem + (paddr - CONFIG_MBASE) / 4; 
+	printf("after guest_to_host\n");}
 
-uint32_t pmem_read_npc(uint32_t addr) {printf("before mem_read_npc!\n");
-  uint32_t ret = host_read(guest_to_host(addr));printf("after pmem_read_npc!\n");
+uint32_t pmem_read_npc(uint32_t addr) {
+	printf("before mem_read_npc!\n");
+	uint32_t ret = host_read(guest_to_host(addr));
+	printf("after pmem_read_npc!\n");
   return ret;
 }
 
@@ -102,7 +107,7 @@ int main(int argc, char** argv, char** env) {
 		// printf("pc = 0x%08x\n",dut.pc);
 		// printf("before pmem_read\n");
 		//dut.inst = pmem_read_npc(dut.pc);
-		//get_inst();
+		get_inst();
 		// printf("dut.inst = 0x%032x\n",dut.inst);
 		// printf("after pmem_read\n");
 		dut.eval();
