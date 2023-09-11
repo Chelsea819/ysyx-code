a1755c4905f1cd23adb3bd92d8f33decef7b41c6
tracer-ysyx <tracer@ysyx.org>
2023-09-11 19:55:13 +0800

    > sim RTL ysyx_22041211 王晓雪 Linux chelsea-virtual-machine 5.19.0-35-generic #36~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Feb 17 15:17:25 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  19:55:13 up 22:49,  2 users,  load average: 0.40, 0.36, 0.20

--- npc/vsrc/ysyx_22041211_Decode.v
+++ npc/vsrc/ysyx_22041211_Decode.v
@@ -30,15 +30,23 @@ module ysyx_22041211_Decode #(parameter DATA_LEN = 32)(
     // 3'b011  R
     // 3'b100  S
 
+    // //tell-opcode 
+    // ysyx_22041211_MuxKeyWithDefault #(6, 7, 3) tell_opcode (key_all[2:0], inst[6:0], 3'b111,{
+    //     7'b0010011 , 3'b000, // 3'b000  I addi sltiu srai andi
+    //     7'b1110011 , 3'b001, // 3'b001  N ecall ebreak 
+    //     7'b0110111 , 3'b010, // 3'b010  U lui
+    //     7'b0010111 , 3'b010, // 3'b010  U auipc
+    //     7'b0110011 , 3'b011, // 3'b011  R add sub
+    //     7'b0100011 , 3'b100  // 3'b100  S sb sw sh
+    // });
+
     //tell-opcode 
-    ysyx_22041211_MuxKeyWithDefault #(6, 7, 3) tell_opcode (key_all[2:0], inst[6:0], 3'b0,{
-        7'b0010011 , 3'b000, // 3'b000  I addi sltiu srai andi
-        7'b1110011 , 3'b001, // 3'b001  N 
-        7'b0110111 , 3'b010, // 3'b010  U lui
-        7'b0010111 , 3'b010, // 3'b010  U auipc
-        7'b0110011 , 3'b011, // 3'b011  R add sub
-        7'b0100011 , 3'b100  // 3'b100  S sb sw sh
-    });
+    assign key_all[2:0] = inst[6:0] == 7'b0010011 ? 3'b000 :                               // 3'b000  I addi sltiu srai andi
+                          inst[6:0] == 7'b1110011 ? 3'b001 :                               // 3'b001  N ecall ebreak  
+                          inst[6:0] == 7'b0110111 || inst[6:0] == 7'b0010111 ? 3'b010 :    // 3'b010  U lui auipc
+                          inst[6:0] == 7'b0110011 ? 3'b011 :                               // 3'b011  R add sub
+                          inst[6:0] == 7'b0100011 ? 3'b100 : 3'b111;                       // 3'b100  S sb sw sh
+
 
     //type_N 识别具体是哪一条指令
     ysyx_22041211_MuxKeyWithDefault #(2, 25, 3) tell_inst (key_all[5:3], inst[31:7], 3'b0,{
