0a0f392b08d3b97c752752253b7b7e118eafdf1f
tracer-ysyx <tracer@ysyx.org>
2023-09-12 13:22:07 +0800

    > sim RTL ysyx_22041211 王晓雪 Linux chelsea-virtual-machine 5.19.0-35-generic #36~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Feb 17 15:17:25 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  13:22:07 up 1 day,  2:09,  2 users,  load average: 0.32, 0.40, 0.30

--- npc/vsrc/ysyx_22041211_RAM.v
+++ npc/vsrc/ysyx_22041211_RAM.v
@@ -4,7 +4,7 @@ module ysyx_22041211_RAM #(parameter ADD_LEN = 32,DATA_LEN = 32)(
 	input		[DATA_LEN - 1:0]				data	,
 	output	    [DATA_LEN - 1:0]				dout	
 );
-	reg			[DATA_LEN - 1:0]		ram_data;
+	wire			[DATA_LEN - 1:0]		ram_data;
 	assign ram_data[addr] = data & {DATA_LEN{w_en}};
 	assign dout = ram_data[addr];
 
