6d165516c6e7a4e09b9f9c6e04fedf65ce0dcf37
tracer-ysyx <tracer@ysyx.org>
2023-09-12 13:02:39 +0800

    > sim RTL ysyx_22041211 王晓雪 Linux chelsea-virtual-machine 5.19.0-35-generic #36~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Feb 17 15:17:25 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  13:02:39 up 1 day,  1:50,  2 users,  load average: 0.27, 0.38, 0.25

--- npc/csrc/sim_main.cpp
+++ npc/csrc/sim_main.cpp
@@ -30,7 +30,7 @@ static uint32_t *pmem = NULL;
 bool ifbreak = false;
 
 void ifebreak_func(int key){
-	printf("while key = %d\n");
+	printf("while key = %d\n",key);
 	if(key == 9) {ifbreak = true; } 
 }
 
