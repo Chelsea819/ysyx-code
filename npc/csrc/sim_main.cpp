37e1d3f0e0ac0424e7519b516fe64a8cac0a7a87
tracer-ysyx <tracer@ysyx.org>
2023-09-12 21:31:47 +0800

    > sim RTL ysyx_22041211 王晓雪 Linux chelsea-virtual-machine 5.19.0-35-generic #36~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Feb 17 15:17:25 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:31:47 up 1 day,  6:04,  2 users,  load average: 0.44, 0.45, 0.49

--- npc/csrc/sim_main.cpp
+++ npc/csrc/sim_main.cpp
@@ -96,11 +96,17 @@ int main(int argc, char** argv, char** env) {
 	dut.trace(m_trace, 5);               
 	m_trace->open("waveform.vcd");
 	dut.rst = 1;
+
+	dut.clk = 0;
+	dut.eval();
+	m_trace->dump(sim_time);
+	sim_time++;
+
 	dut.clk = 1;
 	dut.eval();
 	m_trace->dump(sim_time);
 	sim_time++;
-	
+
 	dut.clk = 0;
 	dut.eval();
 	m_trace->dump(sim_time);
