7983ba001c19657e364d1086f5dadc79da9043d6
tracer-ysyx <tracer@ysyx.org>
2023-09-24 15:59:29 +0800

    > sim RTL ysyx_22041211 王晓雪 Linux chelsea-virtual-machine 5.19.0-35-generic #36~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Feb 17 15:17:25 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:59:29 up 10:22,  2 users,  load average: 0.71, 0.50, 0.47

--- npc/csrc/sim_main.cpp
+++ npc/csrc/sim_main.cpp
@@ -143,6 +143,7 @@ int main(int argc, char** argv, char** env) {
 		dut.clk ^= 1; 
 
 		if(dut.clk == 1) dut.inst = pmem_read_npc(dut.pc);
+			dut.eval();
 		m_trace->dump(sim_time);
 		sim_time++;
 		m_trace->dump(sim_time);
