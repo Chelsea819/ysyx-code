bbd5d0f65e26fc44a0ee6ccf093d512fc478cdd3
tracer-ysyx <tracer@ysyx.org>
2023-11-19 16:17:10 +0800

    > sim RTL ysyx_22041211 王晓雪 Linux chelsea-virtual-machine 5.19.0-35-generic #36~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Feb 17 15:17:25 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:17:10 up 18:48,  2 users,  load average: 1.81, 1.28, 1.05

--- npc/csrc/sim_main.cpp
+++ npc/csrc/sim_main.cpp
@@ -49,6 +49,7 @@ static char *img_file = NULL;
 #define CONFIG_MBASE 0x80000000
 #define CONFIG_PC_RESET_OFFSET 0x0
 #define CONFIG_RT_CHECK 1
+#define CONFIG_ISA_riscv 1
 
 #define CHOOSE2nd(a, b, ...) b
 #define MUX_WITH_COMMA(contain_comma, a, b) CHOOSE2nd(contain_comma a, b)
