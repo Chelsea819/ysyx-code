fd2118e46d45a1a0985c71db4cfb5751ac6141bb
tracer-ysyx <tracer@ysyx.org>
2023-11-19 13:30:51 +0800

    > sim RTL ysyx_22041211 王晓雪 Linux chelsea-virtual-machine 5.19.0-35-generic #36~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Feb 17 15:17:25 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  13:30:51 up 18:12,  3 users,  load average: 1.86, 1.56, 1.41

--- npc/utils/disasm.cc
+++ npc/utils/disasm.cc
@@ -96,9 +96,9 @@ extern "C" void disassemble(char *str, int size, uint64_t pc, uint8_t *code, int
   MCInst inst;
   llvm::ArrayRef<uint8_t> arr(code, nbyte);
   uint64_t dummy_size = 0;
-  printf("111\n");
   gDisassembler->getInstruction(inst, dummy_size, arr, pc, llvm::nulls());
 
+  printf("111\n");
   std::string s;
   raw_string_ostream os(s);
   gIP->printInst(&inst, pc, "", *gSTI, os);
