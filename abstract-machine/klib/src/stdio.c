7599f8a70047f65721bef836fe519c3f5bf28306
tracer-ysyx <tracer@ysyx.org>
2023-12-15 22:37:06 +0800

    > compile NEMU ysyx_22041211 王晓雪 Linux chelsea-virtual-machine 5.19.0-35-generic #36~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Feb 17 15:17:25 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  22:37:06 up 4 days, 12:57, 14 users,  load average: 0.62, 0.69, 0.64

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -6,7 +6,6 @@
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
 int printf(const char *fmt, ...) {
-  // putch('a');
   panic("Not implemented");
 }
 
@@ -17,6 +16,7 @@ int vsprintf(char *out, const char *fmt, va_list ap) {
 
 void convert(int num,int* numAdd,char *arr_tmp){
   int tmp = num % 10;
+  if(tmp < 0){tmp *= -1;}
   arr_tmp[(*numAdd) --] = tmp + 48; 
 }
 
@@ -24,7 +24,6 @@ void convert(int num,int* numAdd,char *arr_tmp){
 //碰到%后，指示%的指针不变 pCurrent先前跑 找到格式化输出的格式
 
 int sprintf(char *out, const char *fmt, ...) {
-  
   va_list ap;
   va_start(ap,fmt); //初始化ap
 
@@ -40,24 +39,33 @@ int sprintf(char *out, const char *fmt, ...) {
     else if(percent == 1 && i == tmp + 1){
       // %d
       if(fmt[i] == 'd'){
-        char arr_tmp[20] = {0};  //存放数字转换成的字符
-        int numAdd = 18;  //数组的下标 从后往前存
+        char arr_tmp[100] = {0};  //存放数字转换成的字符
+        int numAdd = 98;  //数组的下标 从后往前存
+
         int num = va_arg(ap,int); //要替换的数字
         //每一次调用va_arg()都会修改ap，这样下一次调用 就会返回下一个参数
         //va_arg(ap,type) 这个type是为了初始化一个指向目标的指针
         //如果type不匹配或者没有下一个参数，会出现随机错误
-        
-        int flag = 1;  //辅助确定数字的位数
-        while(num / flag != 0){
+
+        int flag = 10; 
+        //负数
+        int neg = 0;
+        if(num < 0){
+          neg = 1;
+        }  
+        while(num != 0){
+          //判断这个负数是否可以直接转换为正数
+          if(num < 0 && num >= -2147483647) num *= -1;
           //将整型数字转换成字符串类型
-          convert(num / flag, &numAdd, arr_tmp);
-          flag *= 10;
+          convert(num, &numAdd, arr_tmp);
+          num /= flag;
         }   
         numAdd ++;
-
         //将数字存入out数组
-        for( ;numAdd < 19 ; k++,numAdd ++){
-          out[k] = arr_tmp[numAdd];
+        for( ;numAdd < 99 ; k++,numAdd ++){
+          //负数      
+          if(neg) out[k] = '-';
+          else {out[k] = arr_tmp[numAdd]; }
         }
       } 
 
