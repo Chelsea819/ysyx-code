8c3a75586fa7d182c586e564747d6cd6dab57856
tracer-ysyx <tracer@ysyx.org>
2024-01-23 00:26:21 +0800

    > sim RTL ysyx_22041211 王晓雪 Linux chelsea-virtual-machine 5.19.0-35-generic #36~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Feb 17 15:17:25 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  00:26:21 up  7:04,  5 users,  load average: 1.51, 1.39, 1.30

--- nemu/src/cpu/difftest/ref.c
+++ nemu/src/cpu/difftest/ref.c
@@ -21,12 +21,13 @@
 // 在DUT host memory的`buf`和REF guest memory的`addr`之间拷贝`n`字节,
 // `direction`指定拷贝的方向, `DIFFTEST_TO_DUT`表示往DUT拷贝, `DIFFTEST_TO_REF`表示往REF拷贝
 __EXPORT void difftest_memcpy(paddr_t addr, void *buf, size_t n, bool direction) {
-  // assert(0);
-
   if(direction == DIFFTEST_TO_REF){
-    memcpy((paddr_t*)(&addr) ,(const paddr_t*)buf ,n);
+    printf("n = %ld\n",n);
+    printf("addr = %lu\n",*((paddr_t*)(addr)));
+    memcpy((paddr_t*)(addr) ,(const paddr_t*)buf ,n);
+
   } else {
-    memcpy((paddr_t*)buf ,(const paddr_t*)(&addr) ,n);
+    memcpy((paddr_t*)buf ,(const paddr_t*)(addr) ,n);
     // assert(0);
   }
 }
