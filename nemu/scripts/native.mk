d9801c8a0395e24682b09d039d60207f0a0c870e
tracer-ysyx <tracer@ysyx.org>
2024-01-22 17:14:39 +0800

    > compile NEMU ysyx_22041211 王晓雪 Linux chelsea-virtual-machine 5.19.0-35-generic #36~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Feb 17 15:17:25 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:14:39 up  2:11,  4 users,  load average: 0.53, 0.58, 0.50

--- nemu/scripts/native.mk
+++ nemu/scripts/native.mk
@@ -25,7 +25,7 @@ $(BINARY): compile_git
 # Some convenient rules
 
 override ARGS ?= --log=$(BUILD_DIR)/nemu-log.txt  #--ftrace=$(BUILD_DIR)/$(ALL)-$(ARCH).elf
-override ARGS +=  --batch #$(ARGS_DIFF)
+override ARGS +=  #--batch #$(ARGS_DIFF)
 
 # Command to execute NEMU
 IMG ?=
