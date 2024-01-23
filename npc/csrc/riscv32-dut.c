66a8c725dc4b676ec7fc88b58ea4299047924fe6
tracer-ysyx <tracer@ysyx.org>
2024-01-23 11:55:59 +0800

    > sim RTL ysyx_22041211 王晓雪 Linux chelsea-virtual-machine 5.19.0-35-generic #36~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Feb 17 15:17:25 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  11:55:59 up  9:20,  5 users,  load average: 0.95, 3.42, 2.76

--- npc/csrc/riscv32-dut.c
+++ npc/csrc/riscv32-dut.c
@@ -2,13 +2,21 @@
 #include <difftest.h>
 #include "reg.h"
 
+#include "svdpi.h"
+#include "Vysyx_22041211_top__Dpi.h"
+#include <verilated.h>
+#include <verilated_vcd_c.h>
+#include "Vysyx_22041211_top.h"
+#include "Vysyx_22041211_top___024root.h"
+
 extern CPU_state cpu;
+extern TOP_NAME dut;
 
 // 把通用寄存器和PC与从DUT中读出的寄存器的值进行比较.
 bool isa_difftest_checkregs(CPU_state *ref_r, vaddr_t pc) {
   if(cpu.pc!= ref_r->pc) return false;
   for(int i = 0; i < 32; i++){
-    if(ref_r->gpr[i] != cpu.gpr[i]) return false;
+    if(ref_r->gpr[i] != R(i)) return false;
   } 
   return true;
 }
