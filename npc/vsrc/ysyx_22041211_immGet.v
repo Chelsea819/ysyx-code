5082510bf0028e856c3b224cf2f5c28e506c0761
tracer-ysyx <tracer@ysyx.org>
2024-01-26 00:38:03 +0800

    > compile NEMU ysyx_22041211 王晓雪 Linux chelsea-virtual-machine 5.19.0-35-generic #36~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Feb 17 15:17:25 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  00:38:03 up 1 day, 12:15,  5 users,  load average: 0.89, 0.75, 0.63

--- npc/vsrc/ysyx_22041211_immGet.v
+++ npc/vsrc/ysyx_22041211_immGet.v
@@ -8,11 +8,11 @@ module ysyx_22041211_immGet #(parameter DATA_LEN = 32)(
 //     wire    [31:0]                  key_tmp;
  
     //imm 
-    assign imm = (inst[6:0] == 7'b0010011 || inst[6:0] == 7'b0010011) ? {{20{inst[31]}},inst[31:20]} :         // 3'b000  I addi sltiu srai andi 
+    assign imm = ((inst[6:0] == 7'b0010011 || inst[6:0] == 7'b0010011) ? {{20{inst[31]}},inst[31:20]} :         // 3'b000  I addi sltiu srai andi 
                  (inst[6:0] == 7'b0110111 || inst[6:0] == 7'b0010111) ? {inst[31:12],{12{1'b0}}} :  // 3'b010  U lui auipc
                  (inst[6:0] == 7'b0100011) ? {{30{inst[31]}},inst[31:25],inst[11:7]} :  // S
                  (inst[6:0] == 7'b1100011) ? {{29{inst[31]}},inst[31],inst[7],inst[30:25],inst[11:8],{1'b0}} :  // B 
-                 inst[6:0] == 7'b1101111 ? {{11{inst[31]}},inst[31],inst[19:12],inst[20],inst[30:21],1'b0}  : 32'b0;   // 3'b101  J 
+                 inst[6:0] == 7'b1101111 ? {{11{inst[31]}},inst[31],inst[19:12],inst[20],inst[30:21],1'b0}  : 32'b0);   // 3'b101  J 
 
 
 endmodule
