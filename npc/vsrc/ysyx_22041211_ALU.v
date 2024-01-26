7b247f73c9575774ac47cda2e40743ca4415c96d
tracer-ysyx <tracer@ysyx.org>
2024-01-26 21:45:35 +0800

    > compile NEMU ysyx_22041211 王晓雪 Linux chelsea-virtual-machine 5.19.0-35-generic #36~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Feb 17 15:17:25 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:45:35 up 1 day, 21:55,  5 users,  load average: 1.17, 0.93, 0.69

--- npc/vsrc/ysyx_22041211_ALU.v
+++ npc/vsrc/ysyx_22041211_ALU.v
@@ -24,7 +24,7 @@ module ysyx_22041211_ALU #(parameter DATA_LEN = 32)(
 
 	assign signed_a = src1;
 	assign signed_b = src2;
-	// assign SF = (alu_control == 4'b0011 || alu_control == 4'b0111 || alu_control == 4'b1101) ? result_tmp[31] : ~cout ;
+	assign SF = (alu_control == 4'b0011 || alu_control == 4'b0111 || alu_control == 4'b1101) ? result_tmp[31] : ~cout ;
 	assign result = (alu_control == 4'b0011) ? {{31{1'b0}}, SF} : 
 					(alu_control == 4'b0100) ? {{31{1'b0}}, ~cout} : result_tmp;
 	wire [31:0] tmp;
@@ -52,13 +52,13 @@ module ysyx_22041211_ALU #(parameter DATA_LEN = 32)(
 	});
 
 	assign zero = result_tmp == 32'b0 ;
-	// assign {cout,tmp} = ((alu_control == 4'b0000) ? (({1'b0,src1} + {1'b0,src2})) :
-	// 					(alu_control == 4'b0001) ? (({1'b0,src1} + {1'b0,~src2} + 1)) :
-	// 			 		(alu_control == 4'b0100) ? (({1'b0,src1} + {1'b0,~src2} + 1)) : 
-	// 					(alu_control == 4'b0011) ? (({1'b0,signed_a} + {1'b0,~signed_b} + 1)) : 33'b0);
+	assign {cout,tmp} = ((alu_control == 4'b0000) ? (({1'b0,src1} + {1'b0,src2})) :
+						(alu_control == 4'b0001) ? (({1'b0,src1} + {1'b0,~src2} + 1)) :
+				 		(alu_control == 4'b0100) ? (({1'b0,src1} + {1'b0,~src2} + 1)) : 
+						(alu_control == 4'b0011) ? (({1'b0,signed_a} + {1'b0,~signed_b} + 1)) : 33'b0);
 
-	assign {SF,cout,tmp} = ((alu_control == 4'b0100) ? (({2'b0,src1} + {2'b0,~src2} + 34'b1)) : 
-						   (alu_control == 4'b0011) ? (({src1[31],1'b0,src1[30:0]} + {src1[31],1'b1,~src1[30:0]} + 1)) : 33'b0);
+	// assign {SF,cout,tmp} = ((alu_control == 4'b0100) ? (({2'b0,src1} + {2'b0,~src2} + 34'b1)) : 
+	// 					   (alu_control == 4'b0011) ? (({src1[31],1'b0,src1[30:0]} + {src1[31],1'b1,~src1[30:0]} + 1)) : 33'b0);
 
 
 
