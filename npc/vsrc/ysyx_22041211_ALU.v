14fcb79292b084ed72fff234d90394a6c2027df3
tracer-ysyx <tracer@ysyx.org>
2024-01-26 14:31:37 +0800

    > compile NEMU ysyx_22041211 王晓雪 Linux chelsea-virtual-machine 5.19.0-35-generic #36~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Feb 17 15:17:25 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  14:31:37 up 1 day, 16:57,  5 users,  load average: 0.75, 0.74, 0.55

--- npc/vsrc/ysyx_22041211_ALU.v
+++ npc/vsrc/ysyx_22041211_ALU.v
@@ -29,6 +29,8 @@ module ysyx_22041211_ALU #(parameter DATA_LEN = 32)(
 	assign result = (alu_control == 4'b0011) ? {{31{1'b0}},{SF}} : 
 					(alu_control == 4'b0100) ? {{31{1'b0}},{(~cout & zero)}} : result_tmp;
 	wire [31:0] tmp;
+	wire [31:0] src2_tmp;
+	assign src2_tmp = ~src2;
 
 	// assign sub = (alu_control == 4'b0001 || alu_control == 4'b0011 || alu_control == 4'b0100);
 
