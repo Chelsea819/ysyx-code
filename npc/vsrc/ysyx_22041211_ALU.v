0cb3b334cc4ec8b03f904f2a0e3377e29b41157a
tracer-ysyx <tracer@ysyx.org>
2024-01-26 14:26:06 +0800

    > compile NEMU ysyx_22041211 王晓雪 Linux chelsea-virtual-machine 5.19.0-35-generic #36~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Feb 17 15:17:25 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  14:26:06 up 1 day, 16:52,  5 users,  load average: 1.36, 0.86, 0.48

--- npc/vsrc/ysyx_22041211_ALU.v
+++ npc/vsrc/ysyx_22041211_ALU.v
@@ -5,6 +5,7 @@
 	> Created Time: 2023年08月04日 星期五 18时24分15秒
  ************************************************************************/
 /* verilator lint_off UNUSEDSIGNAL */
+/* verilator lint_off WIDTHEXPAND */
 module ysyx_22041211_ALU #(parameter DATA_LEN = 32)(
 	input		[DATA_LEN - 1:0]		src1,
 	input		[DATA_LEN - 1:0]		src2,
@@ -58,3 +59,4 @@ module ysyx_22041211_ALU #(parameter DATA_LEN = 32)(
 
 endmodule
 /* verilator lint_on UNUSEDSIGNAL */
+/* verilator lint_on WIDTHEXPAND */
