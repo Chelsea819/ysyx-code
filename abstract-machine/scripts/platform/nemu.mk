50590b992406807671cd4d1b26b5e399dd7d1974
tracer-ysyx <tracer@ysyx.org>
2024-01-29 18:29:25 +0800

    > compile NEMU ysyx_22041211 王晓雪 Linux chelsea-virtual-machine 5.19.0-35-generic #36~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Feb 17 15:17:25 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  18:29:25 up  7:43,  5 users,  load average: 0.61, 0.64, 0.77

--- abstract-machine/scripts/platform/nemu.mk
+++ abstract-machine/scripts/platform/nemu.mk
@@ -6,7 +6,7 @@ AM_SRCS := platform/nemu/trm.c \
            platform/nemu/ioe/audio.c \
            platform/nemu/ioe/disk.c \
            platform/nemu/mpe.c
-
+ALL ?= $(NAME)
 CFLAGS    += -fdata-sections -ffunction-sections
 LDFLAGS   += -T $(AM_HOME)/scripts/linker.ld \
              --defsym=_pmem_start=0x80000000 --defsym=_entry_offset=0x0
