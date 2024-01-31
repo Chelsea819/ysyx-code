112daa709458b6b321fb6f0cd24591616a32a1d4
tracer-ysyx <tracer@ysyx.org>
2024-01-31 12:04:45 +0800

    > compile NEMU ysyx_22041211 王晓雪 Linux chelsea-virtual-machine 5.19.0-35-generic #36~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Feb 17 15:17:25 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  12:04:45 up 16:57,  6 users,  load average: 1.16, 0.90, 0.89

--- abstract-machine/am/src/platform/nemu/ioe/gpu.c
+++ abstract-machine/am/src/platform/nemu/ioe/gpu.c
@@ -16,8 +16,8 @@ void __am_gpu_init() {
   int i;
   // int w = io_read(AM_GPU_CONFIG).width;  // TODO: get the correct width
   // int h = io_read(AM_GPU_CONFIG).height;  // TODO: get the correct height
-  int w = W / N;
-  int h = H / N;
+  int w = W;
+  int h = H;
   uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
   for (i = 0; i < w * h; i ++) fb[i] = i;
   outl(SYNC_ADDR, 1);
