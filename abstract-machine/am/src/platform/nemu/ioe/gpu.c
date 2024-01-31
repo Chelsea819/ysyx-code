c7aaa65e7f4099db1313812534a84c3d05d845b2
tracer-ysyx <tracer@ysyx.org>
2024-01-31 12:06:59 +0800

    > compile NEMU ysyx_22041211 王晓雪 Linux chelsea-virtual-machine 5.19.0-35-generic #36~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Feb 17 15:17:25 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  12:06:59 up 16:59,  6 users,  load average: 0.81, 0.91, 0.90

--- abstract-machine/am/src/platform/nemu/ioe/gpu.c
+++ abstract-machine/am/src/platform/nemu/ioe/gpu.c
@@ -26,7 +26,7 @@ void __am_gpu_init() {
 void __am_gpu_config(AM_GPU_CONFIG_T *cfg) {
   *cfg = (AM_GPU_CONFIG_T) {
     .present = true, .has_accel = false,
-    .width = W * N, .height = H * N,
+    .width = W, .height = H,
     .vmemsz = 0
   };
 }
