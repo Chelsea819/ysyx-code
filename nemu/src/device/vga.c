765f78bfc72b15d9aef9c05e078353e237ddd316
tracer-ysyx <tracer@ysyx.org>
2024-01-31 14:53:27 +0800

    > compile NEMU ysyx_22041211 王晓雪 Linux chelsea-virtual-machine 5.19.0-35-generic #36~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Feb 17 15:17:25 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  14:53:27 up 17:59,  6 users,  load average: 1.17, 1.02, 0.86

--- nemu/src/device/vga.c
+++ nemu/src/device/vga.c
@@ -71,11 +71,12 @@ static inline void update_screen() {
 #endif
 
 void vga_update_screen() {
-  printf("update mmio_read(CONFIG_VGA_CTL_MMIO + 4, 4) %d\n",mmio_read(CONFIG_VGA_CTL_MMIO + 4, 4));
-  if(mmio_read(CONFIG_VGA_CTL_MMIO + 4, 4)) {
-    printf("mmio_read(CONFIG_VGA_CTL_MMIO + 4, 4) %d\n",mmio_read(CONFIG_VGA_CTL_MMIO + 4, 4));
+  printf("vgactl_port_base[1] %d\n",vgactl_port_base[1]);
+  if(vgactl_port_base[1]) {
+    printf("enter vgactl_port_base[1] %d\n",vgactl_port_base[1]);
     update_screen();
-    mmio_write(CONFIG_VGA_CTL_MMIO + 4, 4, 0);
+    vgactl_port_base[1] = 0;
+    // io_write(CONFIG_VGA_CTL_MMIO + 4, 0);
     // io_write(AM_GPU_FBDRAW, 0, 0, vmem, screen_width(), screen_height(), false);
   }
 
@@ -91,7 +92,6 @@ void init_vga() {
 #else
   add_mmio_map("vgactl", CONFIG_VGA_CTL_MMIO, vgactl_port_base, 8, NULL);
 #endif
-
   vmem = new_space(screen_size());
   add_mmio_map("vmem", CONFIG_FB_ADDR, vmem, screen_size(), NULL);
   IFDEF(CONFIG_VGA_SHOW_SCREEN, init_screen());
