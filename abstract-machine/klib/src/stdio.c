8406e8cacf44c41a16a248b2047b2cc1f71910e9
tracer-ysyx <tracer@ysyx.org>
2024-02-02 16:32:40 +0800

    > compile NEMU ysyx_22041211 王晓雪 Linux chelsea-virtual-machine 5.19.0-35-generic #36~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Feb 17 15:17:25 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:32:40 up 1 day,  7:30,  6 users,  load average: 0.62, 0.67, 0.62

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -5,10 +5,10 @@
 
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 #define MAX_SIZE 1024*4
-
+#define NUM_BUF 50
 // -2147483648
-void convert(int num,int* numAdd,char *arr_tmp){
-  int tmp = num % 10; //-8
+void convert(int num,int* numAdd,char *arr_tmp,int flag){
+  int tmp = num % flag; //-8
   if(tmp < 0){tmp *= -1;}
   arr_tmp[(*numAdd)] = tmp + 48; 
   // putch(arr_tmp[(*numAdd)]);
@@ -16,12 +16,22 @@ void convert(int num,int* numAdd,char *arr_tmp){
   (*numAdd) --;
 }
 
+// void convertTo16(int num,int* numAdd,char *arr_tmp){
+//   int tmp = num % 10; //-8
+//   if(tmp < 0){tmp *= -1;}
+//   arr_tmp[(*numAdd)] = tmp + 48; 
+//   // putch(arr_tmp[(*numAdd)]);
+//   // putch('\n');
+//   (*numAdd) --;
+// }
+
 int vsnprintf(char *out, size_t n, const char *fmt, va_list ap) {
   // panic("Not implemented");
 
   int percent = 0; //检测%
   int tmp = 0;     //存放%的下标
   int k = 0;       //out数组的下标
+  int flag = 0;
 
   for(int i = 0; k < n-1 && *(fmt + i) != '\0'; i++){
     if(fmt[i] == '%') {percent ^= 1; tmp = i;}
@@ -31,15 +41,15 @@ int vsnprintf(char *out, size_t n, const char *fmt, va_list ap) {
     else if(percent == 1 && i == tmp + 1){
       // %d
       if(fmt[i] == 'd'){
-        char arr_tmp[100] = {0};  //存放数字转换成的字符
-        int numAdd = 98;  //数组的下标 从后往前存
+        char arr_tmp[NUM_BUF] = {0};  //存放数字转换成的字符
+        int numAdd = NUM_BUF - 2;  //数组的下标 从后往前存
 
         int num = va_arg(ap,int); //要替换的数字
         //每一次调用va_arg()都会修改ap，这样下一次调用 就会返回下一个参数
         //va_arg(ap,type) 这个type是为了初始化一个指向目标的指针
         //如果type不匹配或者没有下一个参数，会出现随机错误
 
-        int flag = 10; 
+        flag = 10; 
         //负数
         int neg = 0;
         if(num < 0){
@@ -49,12 +59,12 @@ int vsnprintf(char *out, size_t n, const char *fmt, va_list ap) {
           //判断这个负数是否可以直接转换为正数
           if(num < 0 && num >= -2147483647) num *= -1;
           //将整型数字转换成字符串类型
-          convert(num, &numAdd, arr_tmp);
+          convert(num, &numAdd, arr_tmp,flag);
           num /= flag;
         }while(num != 0);   
         numAdd ++;
         //将数字存入out数组
-        for( ; k < n-1 && numAdd < 99 ; k++,numAdd ++){
+        for( ; k < n-1 && numAdd < NUM_BUF - 1 ; k++,numAdd ++){
           //负数      
           if(neg) {out[k] = '-'; neg = 0;}
           else {out[k] = arr_tmp[numAdd]; }
@@ -75,7 +85,38 @@ int vsnprintf(char *out, size_t n, const char *fmt, va_list ap) {
         out[k] = c;
         k ++;
       }
-      else{
+
+      else if(fmt[i] == 'x'){
+        char arr_tmp[NUM_BUF] = {0};  //存放数字转换成的字符
+        int numAdd = NUM_BUF - 2;  //数组的下标 从后往前存
+
+        int num = va_arg(ap,int); //要替换的数字
+        //每一次调用va_arg()都会修改ap，这样下一次调用 就会返回下一个参数
+        //va_arg(ap,type) 这个type是为了初始化一个指向目标的指针
+        //如果type不匹配或者没有下一个参数，会出现随机错误
+
+        flag = 16; 
+        //负数
+        int neg = 0;
+        if(num < 0){
+          neg = 1;
+        }  
+        do{
+          //判断这个负数是否可以直接转换为正数
+          if(num < 0 && num >= -2147483647) num *= -1;
+          //将整型数字转换成字符串类型
+          convert(num, &numAdd, arr_tmp,flag);
+          num /= flag;
+        }while(num != 0);   
+        numAdd ++;
+        //将数字存入out数组
+        for( ; k < n-1 && numAdd < NUM_BUF - 1 ; k++,numAdd ++){
+          //负数      
+          if(neg) {out[k] = '-'; neg = 0;}
+          else {out[k] = arr_tmp[numAdd]; }
+        }
+      }
+      else {
         panic("Not completed format");
         return -1;
       }
