1b99675e965dee92679b9f06e6ac3474eb2cc20d
tracer-ysyx <tracer@ysyx.org>
2024-02-02 15:03:16 +0800

    > compile NEMU ysyx_22041211 王晓雪 Linux chelsea-virtual-machine 5.19.0-35-generic #36~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Feb 17 15:17:25 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:03:16 up 1 day,  6:00,  6 users,  load average: 1.29, 1.15, 1.02

--- nemu/include/cpu/ifetch.h
+++ nemu/include/cpu/ifetch.h
@@ -18,7 +18,7 @@
 #include <memory/vaddr.h>
 
 //负责取指， 取指操作的本质只不过就是一次内存的访问而已
-inline uint32_t inst_fetch(vaddr_t *pc, int len) {
+static inline uint32_t inst_fetch(vaddr_t *pc, int len) {
   uint32_t inst = vaddr_ifetch(*pc, len);
   (*pc) += len; //renew 's->snpc'
   return inst;
