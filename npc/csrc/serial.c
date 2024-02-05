de367ef895e2e8d1961716bc092ea39c73aba9d7
tracer-ysyx <tracer@ysyx.org>
2024-02-05 21:11:35 +0800

    > compile NEMU ysyx_22041211 王晓雪 Linux chelsea-virtual-machine 5.19.0-35-generic #36~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Feb 17 15:17:25 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:11:35 up 2 days,  1:22,  5 users,  load average: 1.56, 1.24, 0.73

--- npc/csrc/serial.c
+++ npc/csrc/serial.c
@@ -15,6 +15,7 @@
 
 #include <utils.h>
 #include <device/map.h>
+#include "device-def.h"
 
 /* http://en.wikibooks.org/wiki/Serial_Programming/8250_UART_Programming */
 // NOTE: this is compatible to 16550
