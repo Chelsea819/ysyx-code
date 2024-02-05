36b4a4cf5c05ad175caa3b27b9e0be74c259d9bf
tracer-ysyx <tracer@ysyx.org>
2024-02-05 18:35:15 +0800

    > compile NEMU ysyx_22041211 王晓雪 Linux chelsea-virtual-machine 5.19.0-35-generic #36~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Feb 17 15:17:25 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  18:35:15 up 1 day, 23:02,  5 users,  load average: 4.30, 2.11, 1.33

--- npc/csrc/device/io/map.c
+++ npc/csrc/device/io/map.c
@@ -64,7 +64,7 @@ word_t map_read(paddr_t addr, int len, IOMap *map) {
   check_bound(map, addr);
   paddr_t offset = addr - map->low;
   invoke_callback(map->callback, offset, len, false); // prepare data to read
-  word_t ret = host_read(map->space + offset, len);
+  word_t ret = host_read((paddr_t *)map->space + offset, len);
   return ret;
 }
 
@@ -73,5 +73,5 @@ void map_write(paddr_t addr, int len, word_t data, IOMap *map) {
   check_bound(map, addr);
   paddr_t offset = addr - map->low;
   host_write(map->space + offset, len, data);
-  invoke_callback(map->callback, offset, len, true);
+  invoke_callback((paddr_t *)map->callback, offset, len, true);
 }
