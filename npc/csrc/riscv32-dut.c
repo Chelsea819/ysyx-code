691cf611bb11dfd0830b97982870ce278fb72b1c
tracer-ysyx <tracer@ysyx.org>
2024-02-11 15:44:32 +0800

    > compile NEMU ysyx_22041211 王晓雪 Linux chelsea-virtual-machine 5.19.0-35-generic #36~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Feb 17 15:17:25 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:44:32 up 2 days, 20:50,  6 users,  load average: 2.87, 1.89, 1.38

--- npc/csrc/riscv32-dut.c
+++ npc/csrc/riscv32-dut.c
@@ -2,12 +2,7 @@
 #include <difftest.h>
 #include "reg.h"
 
-#include <svdpi.h>
-#include "Vysyx_22041211_top__Dpi.h"
-#include <verilated.h>
-#include <verilated_vcd_c.h>
-#include "Vysyx_22041211_top.h"
-#include "Vysyx_22041211_top___024root.h"
+#include "sim.h"
 
 extern CPU_state cpu;
 extern TOP_NAME dut;
