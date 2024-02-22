341bc63f9cc32694052ce0fba4104dd06315e924
tracer-ysyx <tracer@ysyx.org>
2024-02-22 16:30:26 +0800

    > sim RTL ysyx_22041211 王晓雪 Linux chelsea-virtual-machine 5.19.0-35-generic #36~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Feb 17 15:17:25 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:30:26 up 5 days,  4:41,  8 users,  load average: 0.92, 0.95, 0.90

--- abstract-machine/klib/src/stdlib.c
+++ abstract-machine/klib/src/stdlib.c
@@ -44,9 +44,9 @@ void *malloc(size_t size) {
     addr = (char *)heap.start;
   void *hbrk = addr;
   addr = addr + size;
-  printf("heap.start = 0x%08x\n",heap.start);
-  printf("addr = 0x%08x\n",addr);
-  printf("heap.end = 0x%08x\n",heap.end);
+  printf("heap.start = 0x%x\n",heap.start);
+  printf("addr = 0x%x\n",addr);
+  printf("heap.end = 0x%x\n",heap.end);
   assert((uintptr_t)heap.start <= (uintptr_t)addr && (uintptr_t)addr < (uintptr_t)heap.end);
   return hbrk;
 #endif
