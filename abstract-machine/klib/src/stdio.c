327ab14db2c1f027512194b3bc0cd756c47be87e
tracer-ysyx <tracer@ysyx.org>
2024-02-25 20:13:32 +0800

    > sim RTL ysyx_22041211 王晓雪 Linux chelsea-virtual-machine 5.19.0-35-generic #36~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Feb 17 15:17:25 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:13:32 up 5 days,  7:17,  8 users,  load average: 0.66, 0.71, 0.99

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -1,212 +1,249 @@
+#include <stdarg.h>
+
 #include <am.h>
 #include <klib.h>
 #include <klib-macros.h>
-#include <stdarg.h>
 
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
-#define MAX_SIZE 1024*4
-#define NUM_BUF 50
-// -2147483648
-void convert(int num,int* numAdd,char *arr_tmp,int flag){
-  int tmp = num % flag; //-8
-  if(tmp < 0){tmp *= -1;}
-  arr_tmp[(*numAdd)] = tmp + 48; 
-  // putch(arr_tmp[(*numAdd)]);
-  // putch('\n');
-  (*numAdd) --;
-}
 
-void intHandel(int *neg, int *num, int *numAdd, int flag, char *arr_tmp){
-  //负数
-  if(*num < 0){
-    *neg = 1;
-  }  
-  do{
-    //判断这个负数是否可以直接转换为正数
-    if(*num < 0 && *num >= -2147483647) *num *= -1;
-    //将整型数字转换成字符串类型
-    convert(*num, numAdd, arr_tmp,flag);
-    *num /= flag;
-  }while(*num != 0);   
-  (*numAdd) ++;
-}
+#define BUF_SIZE 1024*4
 
-void formatHandel(bool *if_wid, bool *if_for, int numAdd, int *width, char *out, int *k){
-  if(*if_wid && NUM_BUF - numAdd - 1 < *width){
-    for(int i = *width - (NUM_BUF - numAdd - 1); i > 0 ; i --, (*k) ++)
-      if(*if_for) out[*k] = '0';
-      else out[*k] = ' ';
-    *if_for = false;
-    *if_wid = false;
-    *width = 0;
+// 鐢ㄤ簬鍙嶈浆鎸囧畾瀛楃涓?
+static void reverse(char *str, int len) {
+  assert(NULL != str);
+  assert(0 < len);
+
+  char *end = str + len - 1;
+  char tmp;
+  while (str < end)
+  {
+    tmp = *str;
+    *str = *end;
+    *end = tmp;
+    str++;
+    end--;
   }
+}
+
+// 鏃犵鍙锋暟itoa
+static int uitoa(unsigned int num, char *str, int base) {
+  assert(NULL != str);
+  assert(16 >= base);
+  assert(0 < base);
+
+  int i = 0;
+  int bit;
+  do {
+    bit = num % base;
+    if (10 <= bit) str[i++] = 'a' + bit -10;
+    else str[i++] = '0' + bit;
+  } while ((num /= base) > 0);
+  str[i] = '\0';
+  reverse(str, i);
+  
+  return i;
+}
+
+// 绗﹀彿鏁癷toa
+static int itoa(int num, char *str, int base) {
+  assert(NULL != str);
+  assert(16 >= base);
+  assert(0 < base);
+
+  int i = 0;
+  int sign = num;
+  int bit;
+  if (sign < 0) num = -num;
+  do {
+    bit = num % base;
+    if (10 <= bit) str[i++] = 'a' + bit - 10;
+    else str[i++] = '0' + bit;
+  } while ((num /= base) > 0);
+  if (sign <  0) str[i++] = '-';
+  str[i] = '\0';
+  reverse(str, i);
+
+  return i;
+}
+
+static int intlen(int num, int base) {
+  assert(16 >= base);
+  assert(0 < base);
+
+  int i = 0;
+  int sign = num;
+  if (sign < 0) num = -num;
+  do {
+    i++;
+  } while ((num /= base) > 0);
+  if (sign < 0) i++;
+  return i;
+}
+
+int offset(char *out, size_t *n, size_t padding) {
+  assert(NULL != out);
+  assert(NULL != n);
+
+  char *start = out;
+  size_t this_n = *n;
+  while (0 < padding && 0 < this_n) {
+    *out++ = ' ';
+    this_n--;
+    padding--;
+  }
+  *n = this_n;
+  return out - start;
 }
 
 int vsnprintf(char *out, size_t n, const char *fmt, va_list ap) {
-  // panic("Not implemented");
-  int percent = 0; //检测%
-  // int tmp = 0;     //存放%的下标
-  int k = 0;       //out数组的下标
-  int flag = 0;
-  int width = 0;
-  int neg = 0;
-  bool if_wid = false;
-  bool if_for = false;
-  bool if_long = false;
+  assert(NULL != out);
+  assert(NULL != fmt);
 
-  for(int i = 0; k < n-1 && *(fmt + i) != '\0'; i++){
-    // if(fmt[i] == '%') {putch(fmt[i]);percent ^= 1; tmp = i;}
-    if(fmt[i] == '%') {percent ^= 1; }
+  char *start = out;
+  n--; // 鐣欎竴涓瓧鑺傜粰瀛楃涓茬粨灏剧殑'\0'
+  int val;
+  size_t off_len;
+  const char *str;
 
-    //当percent为1时进入循环,即出现奇数个`%`
-    //匹配到`%`后面的格式化输出标识符
-    else if(percent == 1){
-      // putch(fmt[i]);
-      //%s
-      if(fmt[i] == 's'){
-        percent = 0;
-        char *str = va_arg(ap,char*);
-        for(int j = 0; k < n-1 && str[j] != '\0'; j++,k++){
-          // putch(fmt[i]);
-          out[k] = str[j];
-        }
-      }
-
-      // %c
-      else if(fmt[i] == 'c'){
-        // putch(fmt[i]);
-        percent = 0;
-        char c = va_arg(ap,int);
-        out[k] = c;
-        k ++;
-      }
-      else if(fmt[i] == '0' && !if_for && !if_wid){
-        // putch('f');
-        if_for = true;
-      }
-      else if(fmt[i] >= '0' && fmt[i] <= '9' && !if_wid){
-        // putch('w');
-        if(!if_wid)
-          width = fmt[i] - '0';
-        else
-          width = width * 10 + (fmt[i] - '0');
-        if_wid = true;
-      }
-      else if(if_for && !if_wid)
-        panic("Invalid format");
-      else if(fmt[i] == 'l'){
-        if_long = true;
-      }
-      // %d
-      else if(fmt[i] == 'd'){
-        // putch(fmt[i]);
-        percent = 0;
-        char arr_tmp[NUM_BUF] = {0};  //存放数字转换成的字符
-        int numAdd = NUM_BUF - 2;  //数组的下标 从后往前存
-
-        int num = va_arg(ap,int); //要替换的数字
-        //每一次调用va_arg()都会修改ap，这样下一次调用 就会返回下一个参数
-        //va_arg(ap,type) 这个type是为了初始化一个指向目标的指针
-        //如果type不匹配或者没有下一个参数，会出现随机错误
-        flag = 10; 
-        neg = 0;
-        intHandel(&neg, &num, &numAdd, flag, arr_tmp);
-        formatHandel(&if_wid, &if_for, numAdd, &width, out, &k);
-        //将数字存入out数组
-        for( ; k < n-1 && numAdd < NUM_BUF - 1 ; k++,numAdd ++){
-          //负数      
-          if(neg) {out[k] = '-'; neg = 0;}
-          else {out[k] = arr_tmp[numAdd]; }
-        }
-        if(if_long) if_long = false;
-      } 
-
+  while ('\0' != *fmt && 0 < n) {
+    if ('%' == *fmt) {
+      fmt++; // 璺宠繃'%'
       
+      // 澶勭悊鏍煎紡鍖栨爣蹇?
+      int leftJustify = 0;
+      if ('-' == *fmt) {
+        leftJustify = 1;
+        fmt++;
+      }
 
-      else if(fmt[i] == 'x'){
-        percent = 0;
-        char arr_tmp[NUM_BUF] = {0};  //存放数字转换成的字符
-        int numAdd = NUM_BUF - 2;  //数组的下标 从后往前存
+      // 澶勭悊瀛楁瀹藉害
+      int width = 0;
+      while ('0' <= *fmt && '9' >= *fmt) {
+        width = width * 10 + (*fmt - '0');
+        fmt++;
+      }
 
-        int num = va_arg(ap,int); //要替换的数字
-        //每一次调用va_arg()都会修改ap，这样下一次调用 就会返回下一个参数
-        //va_arg(ap,type) 这个type是为了初始化一个指向目标的指针
-        //如果type不匹配或者没有下一个参数，会出现随机错误
+      // // 澶勭悊鏍煎紡鍖栫被鍨?
+      if ('l' == *fmt) {
+        fmt++; // FIXME锛氬湪杩欓噷閫夋嫨浜嗚烦杩?l'浣嗘槸涓嶇‘瀹氭槸涓嶆槸姝ｇ‘鐨勫仛娉?
+      }
 
-        flag = 16; 
-        //负数
-        neg = 0;
-        intHandel(&neg, &num, &numAdd, flag, arr_tmp);
-        formatHandel(&if_wid, &if_for, numAdd, &width, out, &k);
-
-        //将数字存入out数组
-        for( ; k < n-1 && numAdd < NUM_BUF - 1 ; k++,numAdd ++){
-          //负数      
-          if(neg) {out[k] = '-'; neg = 0;}
-          else {out[k] = arr_tmp[numAdd]; }
+      // 澶勭悊鏍煎紡鍖栧瓧绗︿覆
+      switch (*fmt) {
+        case '%': {
+          *out++ = *fmt++; 
+          n--; 
+          break;
         }
-        if(if_long) if_long = false;
+        case 'd': {
+          val = va_arg(ap, int);
+          off_len = intlen(val, 10);
+
+          if (0 < width && !leftJustify) out += offset(out, &n, width - off_len);
+          out += itoa(val, out ,10);
+          n -= off_len;
+          if (0 < width && leftJustify) out += offset(out, &n, width - off_len);
+          fmt++;
+
+          break;
+        }
+        case 'x': {
+          val = va_arg(ap, int); // FIXME: 杩欎釜stdio涓墍鏈夌殑涓巌nt鏈夊叧鐨勶紝鏈夊彲鑳借秴杩囧彉閲忕殑鑼冨洿绛夋剰澶栨儏鍐碉紝娉ㄦ剰妫€鏌ワ紝鐩墠灏氭病鏈夋兂鍒版湁浠€涔堟柟娉曡В鍐?
+          off_len = intlen(val, 16); // FIXME: 鍐欑殑鐪熼浮宸村睅锛屽彲浠ョ洿鎺ュitoa鍋氬鐢?
+
+          if (0 < width && !leftJustify && 0 < width - off_len) out += offset(out, &n, width - off_len);
+          out += itoa(val, out, 16);
+          n -= off_len;
+          if (0 < width && leftJustify && 0 < width - off_len) out += offset(out, &n, width - off_len);
+          fmt++;
+
+          break;
+        }
+        case 'p': {
+          *out++ = '0'; 
+          n--;
+          *out++ = 'x'; 
+          n--;
+
+          val = va_arg(ap, int);
+          off_len = intlen(val, 16);
+
+          if (0 < width && !leftJustify && 0 < width - off_len) out += offset(out, &n, width - off_len);
+          out += uitoa(val, out, 16);
+          n -= off_len;
+          if (0 < width && leftJustify && 0 < width - off_len) out += offset(out, &n, width - off_len);
+          fmt++;
+
+          break;
+        }
+        case 'c': {
+          *out++ = va_arg(ap, int); 
+          n--;
+          fmt++;
+          break;
+        }
+        case 's': {
+          str = va_arg(ap, const char*);
+          off_len = strlen(str);
+    
+          if (0 < width && !leftJustify && 0 < width - off_len) out += offset(out, &n, width - off_len);
+          while (*str && 0 < n) {
+            *out++ = *str++;
+            n--;
+          }
+          if (0 < width && leftJustify && 0 < width - off_len) out += offset(out, &n, width - off_len);
+          fmt++;
+
+          break;
+        }
+        default: {
+          *out++ = *fmt++; 
+          n--; 
+          break;
+        }
       }
-      else {
-        putch(fmt[i]);
-        panic("Not completed format");
-        return -1;
-      }
-      
+    } else { // 闈炴牸寮忓寲瀛楃锛岀洿鎺ュ鍒跺埌缂撳啿鍖?
+      *out++ = *fmt++;
+      n--;
     }
-    else {
-      // putch(fmt[i]);
-      out[k] = fmt[i];
-      k++;
-    } 
   }
-  out[k] = '\0';
-  va_end(ap);
-  return k;
+  *out = '\0';
+  return out - start;
 }
 
 int printf(const char *fmt, ...) {
   va_list ap;
-  va_start(ap,fmt); //初始化ap
-  char out[MAX_SIZE];
-  int ret = vsprintf(out, fmt, ap);
-  assert(ret <= MAX_SIZE && ret >= 0);
-  for(int i = 0; i <= ret; i ++){
+  va_start(ap, fmt);
+  char out[BUF_SIZE];
+  // DESCRIPTION:鍦ㄩ€欒鎴戝仛浜嗕竴鍊嬪Ε鍗旓紝鎴戜笉鍘诲仛鍒ゆ柗鏄惁鎵撳嵃瀹岀暍涓︿笖鍦╞uf婊跨殑鏅傚€欏埛鏂板啀瀵叆銆?
+  // 鎴戦伕鎿囧彧鍏佽ū鍑芥暩鍚慴uf涓鍏UFF_SIZE澶у皬鐨勬暩鎿氾紝涓︿笖鍙厑瑷辨墦鍗伴€欓杭澶氥€傛帯鐢ㄤ簡KISS鍘熷墖
+  int length = vsprintf(out, fmt, ap);
+  assert(length <= BUF_SIZE);
+  for (int i = 0; i < length; i++) {
     putch(out[i]);
   }
   va_end(ap);
-  return ret;
-  // panic("Not implemented");
+  return length;
 }
-
-int vsprintf(char *out, const char *fmt, va_list ap) {
-  // panic("Not implemented");
-  int ret = vsnprintf(out, MAX_SIZE, fmt, ap);
-  return ret;
-}
-
-//碰到%后，指示%的指针不变 pCurrent先前跑 找到格式化输出的格式
 
 int sprintf(char *out, const char *fmt, ...) {
   va_list ap;
-  va_start(ap,fmt); //初始化ap
-
-  int ret = vsprintf(out, fmt, ap);
-
+  va_start(ap, fmt);
+  int length = vsprintf(out, fmt, ap);
   va_end(ap);
-  return ret;
+  return length;
 }
 
 int snprintf(char *out, size_t n, const char *fmt, ...) {
-  // panic("Not implemented");
   va_list ap;
-  va_start(ap,fmt); //初始化ap
-
-  int ret = vsnprintf(out, n, fmt, ap);
-
+  va_start(ap, fmt);
+  int length = vsnprintf(out, n, fmt, ap);
   va_end(ap);
-  return ret;
+  return length;
 }
 
+int vsprintf(char *out, const char *fmt, va_list ap) {
+  return vsnprintf(out, BUF_SIZE, fmt, ap);
+}
 
 #endif
