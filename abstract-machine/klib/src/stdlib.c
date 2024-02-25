7d9a641f56341a3bd29d81dcda1a414cfaa022e6
tracer-ysyx <tracer@ysyx.org>
2024-02-25 16:27:16 +0800

    > sim RTL ysyx_22041211 王晓雪 Linux chelsea-virtual-machine 5.19.0-35-generic #36~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Feb 17 15:17:25 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:27:16 up 5 days,  5:22,  8 users,  load average: 0.85, 0.88, 1.00

--- abstract-machine/klib/src/stdlib.c
+++ abstract-machine/klib/src/stdlib.c
@@ -1,11 +1,13 @@
 #include <am.h>
 #include <klib.h>
 #include <klib-macros.h>
+#include <stdio.h>
 
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 static unsigned long int next = 1;
+
 #if !(defined(__ISA_NATIVE__) && defined(__NATIVE_USE_KLIB__))
-  static void * addr = NULL;
+  static char * addr = NULL;
 #endif
 
 int rand(void) {
@@ -37,13 +39,14 @@ void *malloc(size_t size) {
   // Therefore do not call panic() here, else it will yield a dead recursion:
   //   panic() -> putchar() -> (glibc) -> malloc() -> panic()
 #if !(defined(__ISA_NATIVE__) && defined(__NATIVE_USE_KLIB__))
-  if(addr == NULL){
-    addr = heap.start;
-  }
-  void * ptr = addr;
-  addr = (char*)addr + size;
-  
-  return ptr;
+  if(!size) return NULL;
+  if(addr == NULL)
+    addr = (char *)heap.start;
+  void *hbrk = (void *)addr;
+  addr = addr + size;
+  assert((uintptr_t)heap.start <= (uintptr_t)addr);
+  assert((uintptr_t)heap.start <= (uintptr_t)addr && (uintptr_t)addr < (uintptr_t)heap.end);
+  return hbrk;
 #endif
   return NULL;
 }
