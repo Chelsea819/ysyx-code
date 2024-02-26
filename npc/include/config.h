443a401cb3a50186b0bf38be9136719fb65d9d03
tracer-ysyx <tracer@ysyx.org>
2024-02-26 16:48:08 +0800

    > compile NEMU ysyx_22041211 王晓雪 Linux chelsea-virtual-machine 5.19.0-35-generic #36~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Feb 17 15:17:25 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:48:08 up 5 days, 14:12,  8 users,  load average: 2.94, 2.76, 1.95

--- npc/include/config.h
+++ npc/include/config.h
@@ -4,8 +4,8 @@
 // #define CONFIG_MTRACE 1
 #define CONFIG_FTRACE 1
 #define CONFIG_DIFFTEST 1
-// #define CONFIG_ITRACE_COND 1
-// #define CONFIG_ITRACE 1
+#define CONFIG_ITRACE_COND 1
+#define CONFIG_ITRACE 1
 #define CONFIG_TRACE 1
 #define CONFIG_DEVICE 1
 // #define CONFIG_DTRACE 1
