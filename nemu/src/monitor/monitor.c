8ed4a3e15ab922a375587e24b4a63a185295eaf0
tracer-ysyx <tracer@ysyx.org>
2024-02-26 20:19:12 +0800

    > compile NEMU ysyx_22041211 王晓雪 Linux chelsea-virtual-machine 5.19.0-35-generic #36~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Feb 17 15:17:25 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:19:12 up 5 days, 16:17,  8 users,  load average: 2.43, 2.23, 1.76

--- nemu/src/monitor/monitor.c
+++ nemu/src/monitor/monitor.c
@@ -138,6 +138,7 @@ void init_monitor(int argc, char *argv[]) {
   /* Initialize differential testing. */
   init_difftest(diff_so_file, img_size, difftest_port);
 
+
   /* Initialize the simple debugger.初始化简单调试器 */
   init_sdb();
 
