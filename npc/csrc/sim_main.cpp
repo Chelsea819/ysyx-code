04776515ae7e822c65d4a6a9f143c3e3229f54c8
tracer-ysyx <tracer@ysyx.org>
2024-02-26 12:02:25 +0800

    > compile NEMU ysyx_22041211 王晓雪 Linux chelsea-virtual-machine 5.19.0-35-generic #36~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Feb 17 15:17:25 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  12:02:25 up 5 days, 10:47,  7 users,  load average: 1.16, 0.97, 0.90

--- npc/csrc/sim_main.cpp
+++ npc/csrc/sim_main.cpp
@@ -565,7 +565,7 @@ void init_npc(int argc,char *argv[]){
 }
 
 void set_npc_state(int state, vaddr_t pc, int halt_ret) {
-//   difftest_skip_ref();
+  difftest_skip_ref();
   npc_state.state = state;
   npc_state.halt_pc = pc;
   npc_state.halt_ret = halt_ret;
