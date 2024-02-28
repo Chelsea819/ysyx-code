a494676d3888d977aa85826777e1472501e0818f
tracer-ysyx <tracer@ysyx.org>
2024-02-28 21:14:42 +0800

    > compile NEMU ysyx_22041211 王晓雪 Linux chelsea-virtual-machine 5.19.0-35-generic #36~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Feb 17 15:17:25 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:14:42 up 14:01,  5 users,  load average: 1.51, 1.37, 1.14

--- nemu/src/isa/riscv32/system/intr.c
+++ nemu/src/isa/riscv32/system/intr.c
@@ -27,7 +27,7 @@ word_t isa_raise_intr(word_t NO, vaddr_t epc) {
   cpu.csr[CSR_MCAUSE] = NO;
   cpu.pc = cpu.csr[CSR_MTVEC];
   // printf("cpu.pc = 0x%08x\n",cpu.pc);
-  // printf("cpu.csr[CSR_MTVEC] = 0x%08x\n",cpu.csr[CSR_MTVEC]);
+  printf("cpu.csr[CSR_MTVEC] = 0x%08x\n",cpu.csr[CSR_MTVEC]);
   // printf("cpu.csr[773] = 0x%08x\n",csr(773));
 
   return epc;
