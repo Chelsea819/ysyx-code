4247753ce0587f7a42ea111a11415a5c593a376b
tracer-ysyx <tracer@ysyx.org>
2024-02-29 17:42:38 +0800

    > compile NEMU ysyx_22041211 王晓雪 Linux chelsea-virtual-machine 5.19.0-35-generic #36~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Feb 17 15:17:25 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:42:38 up 21:16,  4 users,  load average: 1.57, 1.15, 0.87

--- nemu/src/isa/riscv32/system/intr.c
+++ nemu/src/isa/riscv32/system/intr.c
@@ -14,12 +14,12 @@
 ***************************************************************************************/
 
 #include <isa.h>
-const char *regs2[] = {
-  "$0", "ra", "sp", "gp", "tp", "t0", "t1", "t2",
-  "s0", "s1", "a0", "a1", "a2", "a3", "a4", "a5",
-  "a6", "a7", "s2", "s3", "s4", "s5", "s6", "s7",
-  "s8", "s9", "s10", "s11", "t3", "t4", "t5", "t6"
-};
+// const char *regs2[] = {
+//   "$0", "ra", "sp", "gp", "tp", "t0", "t1", "t2",
+//   "s0", "s1", "a0", "a1", "a2", "a3", "a4", "a5",
+//   "a6", "a7", "s2", "s3", "s4", "s5", "s6", "s7",
+//   "s8", "s9", "s10", "s11", "t3", "t4", "t5", "t6"
+// };
 
 word_t isa_raise_intr(word_t NO, vaddr_t epc) {
   /* TODO: Trigger an interrupt/exception with ``NO''.
@@ -28,18 +28,18 @@ word_t isa_raise_intr(word_t NO, vaddr_t epc) {
   // SR[mepc] <- PC
   // SR[mcause] <- 一个描述失败原因的号码
   // PC <- SR[mtvec]
-  for(int i = 0; i < 32; i++){
-    printf("\033[104m %d %s: \033[0m \t0x%08x\n",i,regs2[i],cpu.gpr[i]);
-  }
-  printf("\033[102m PC: \033[0m \t0x%08x\n",cpu.pc);
+  // for(int i = 0; i < 32; i++){
+  //   printf("\033[104m %d %s: \033[0m \t0x%08x\n",i,regs2[i],cpu.gpr[i]);
+  // }
+  // printf("\033[102m PC: \033[0m \t0x%08x\n",cpu.pc);
 
   cpu.csr[CSR_MEPC] = epc;
   cpu.csr[CSR_MCAUSE] = NO;
   cpu.pc = cpu.csr[CSR_MTVEC];
   // printf("cpu.pc = 0x%08x\n",cpu.pc);
-  printf("cpu.csr[CSR_MTVEC] = 0x%08x\n",cpu.csr[CSR_MTVEC]);
-  printf("cpu.csr[CSR_MEPC]: 0x%08x\n",cpu.csr[CSR_MEPC]);
-  printf("cpu.csr[CSR_MCAUSE]: 0x%08x\n",cpu.csr[CSR_MCAUSE]);
+  // printf("cpu.csr[CSR_MTVEC] = 0x%08x\n",cpu.csr[CSR_MTVEC]);
+  // printf("cpu.csr[CSR_MEPC]: 0x%08x\n",cpu.csr[CSR_MEPC]);
+  // printf("cpu.csr[CSR_MCAUSE]: 0x%08x\n",cpu.csr[CSR_MCAUSE]);
   // printf("cpu.csr[773] = 0x%08x\n",csr(773));
 
   return cpu.csr[CSR_MTVEC];
