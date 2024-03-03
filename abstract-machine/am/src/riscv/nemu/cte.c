079bf9841f49cb430ffecd007a6763575a7edb9e
tracer-ysyx <tracer@ysyx.org>
2024-03-03 12:47:18 +0800

    > compile NEMU ysyx_22041211 王晓雪 Linux chelsea-virtual-machine 5.19.0-35-generic #36~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Feb 17 15:17:25 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  12:47:18 up 1 day,  8:28,  5 users,  load average: 0.99, 0.89, 0.95

--- abstract-machine/am/src/riscv/nemu/cte.c
+++ abstract-machine/am/src/riscv/nemu/cte.c
@@ -24,8 +24,8 @@ Context* __am_irq_handle(Context *c) {
     // printf("c->mcause: 0x%08x\n",c->mcause);
     // printf("c->mstatus: 0x%08x\n",c->mstatus);
     // printf("c->mepc: 0x%08x\n",c->mepc);
-    c = user_handler(ev, c); // 根据不同事件进行不同操作
     c->mepc += 4;
+    c = user_handler(ev, c); // 根据不同事件进行不同操作
     assert(c != NULL);
   }
 
