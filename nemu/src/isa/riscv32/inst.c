69ed72882d6cf3d23fad11ae5e879960191fdf25
tracer-ysyx <tracer@ysyx.org>
2024-03-05 20:34:40 +0800

    > compile NEMU ysyx_22041211 王晓雪 Linux chelsea-virtual-machine 5.19.0-35-generic #36~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Feb 17 15:17:25 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:34:40 up 2 days,  2:02,  5 users,  load average: 1.46, 1.10, 1.00

--- nemu/src/isa/riscv32/inst.c
+++ nemu/src/isa/riscv32/inst.c
@@ -116,7 +116,7 @@ static int decode_exec(Decode *s) {
   INSTPAT("0000000 ????? ????? 110 ????? 01100 11", or      ,R, R(rd) = src1 | src2 ); 
   INSTPAT("0000000 ????? ????? 111 ????? 01100 11", and    , R, R(rd) = src1 & src2 ); 
   INSTPAT("0000000 00001 00000 000 00000 11100 11", ebreak , I, NEMUTRAP(s->pc, R(10)));
-  INSTPAT("0000000 00000 00000 000 00000 11100 11", ecall  , I, s->dnpc = isa_raise_intr(R(17), s->pc));
+  INSTPAT("0000000 00000 00000 000 00000 11100 11", ecall  , I, s->dnpc = isa_raise_intr(0xb, s->pc));
   INSTPAT("0011000 00010 00000 000 00000 11100 11", mret   , I, s->dnpc = C(CSR_MEPC));
 
   INSTPAT("??????? ????? ????? 010 ????? 11100 11", csrrs  , I, R(rd) = C(BITS(imm, 12, 0)), C(BITS(imm, 12, 0)) |= src1);
@@ -148,7 +148,7 @@ int isa_exec_once(Decode *s) {
   s->isa.inst.val = inst_fetch(&s->snpc, 4);
   // int ret = decode_exec(s);
   // if((s->isa.inst.val & 0x0000007f)== 115) {printf("C(773) = 0x%08x\n",C(773));}
-  // if(s->pc != 0x800000a0 && s->pc != 0x800000a4 && s->pc != 0x80000098 && s->pc != 0x800000a8 && s->pc != 0x8000009c) printf("s->pc = 0x%08x s->isa.inst.val = 0x%08x\n",s->pc,s->isa.inst.val);
+  if(s->pc != 0x800000a0 && s->pc != 0x800000a4 && s->pc != 0x80000098 && s->pc != 0x800000a8 && s->pc != 0x8000009c) printf("s->pc = 0x%08x s->isa.inst.val = 0x%08x\n",s->pc,s->isa.inst.val);
   // if(s->pc == 0x800013d0) assert(0);
   return decode_exec(s);
 }
