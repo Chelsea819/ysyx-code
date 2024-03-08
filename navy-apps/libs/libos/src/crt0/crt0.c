6bc5ff60fe93dcd32ab7f418969836d75e7ba505
tracer-ysyx <tracer@ysyx.org>
2024-03-08 15:00:30 +0800

    > compile NEMU ysyx_22041211 王晓雪 Linux chelsea-virtual-machine 5.19.0-35-generic #36~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Feb 17 15:17:25 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:00:30 up 2 days, 21:13,  5 users,  load average: 1.56, 1.10, 0.83

--- navy-apps/libs/libos/src/crt0/crt0.c
+++ navy-apps/libs/libos/src/crt0/crt0.c
@@ -7,9 +7,6 @@ extern char **environ;
 void call_main(uintptr_t *args) {
   char *empty[] =  {NULL };
   environ = empty;
-  int ret = main(0, empty, empty);
-  assert(ret == 1);
-  exit(4);
-  
+  exit(main(0, empty, empty));
   assert(0);
 }
