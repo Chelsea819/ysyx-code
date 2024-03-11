99258423c88af2da8e1054709647a472c9ef9e24
tracer-ysyx <tracer@ysyx.org>
2024-03-11 11:19:36 +0800

    > compile NEMU ysyx_22041211 王晓雪 Linux chelsea-virtual-machine 5.19.0-35-generic #36~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Feb 17 15:17:25 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  11:19:36 up 3 days,  8:03,  5 users,  load average: 1.32, 1.19, 1.16

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -11,10 +11,17 @@ void sys_exit(){
   halt(0);
 }
 
-// uintptr_t sys_write(){
-//   yield();
-//   return 0;
-// }
+uintptr_t sys_write(Context *c){
+  // 检查fd的值
+  int i = 0;
+  if(c->GPR2 == 1 || c->GPR2 == 2){
+    for(i = 0; i < c->GPR4; i ++){
+      if(!c->GPR2) return -1;
+      putch(*((char *)c->GPR2 + i));
+    }
+  } 
+  return i;
+}
 
 
 void do_syscall(Context *c) {
@@ -27,7 +34,7 @@ void do_syscall(Context *c) {
   switch (a[0]){
     case SYS_exit : strcpy(name,"SYS_exit"); break;
     case SYS_yield: strcpy(name,"SYS_yield"); break;
-    
+    case SYS_write: strcpy(name,"SYS_write"); break;
     // case x: strcpy(name,"SYS_write"); break;
     default: panic("Unhandled syscall ID = %d", a[0]);
   }
@@ -37,7 +44,7 @@ void do_syscall(Context *c) {
   switch (a[0]){
     case SYS_exit: sys_exit(); break;
     case SYS_yield: ret = sys_yield(); break;
-    // case x: ret = sys_write(); break;
+    case SYS_write: ret = sys_write(c); break;
     default: panic("Unhandled syscall ID = %d", a[0]);
   }
   c->GPRx = ret;
