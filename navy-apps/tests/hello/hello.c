a886ff34dccb5b03444488f89da441d2d6da15d6
tracer-ysyx <tracer@ysyx.org>
2024-03-11 13:50:37 +0800

    > compile NEMU ysyx_22041211 王晓雪 Linux chelsea-virtual-machine 5.19.0-35-generic #36~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Feb 17 15:17:25 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  13:50:37 up 3 days,  8:27,  5 users,  load average: 0.86, 0.86, 0.96

--- navy-apps/tests/hello/hello.c
+++ navy-apps/tests/hello/hello.c
@@ -2,14 +2,14 @@
 #include <stdio.h>
 
 int main() {
-  write(1, "Eello World!\n", 13);
+  write(1, "Aello World!\n", 13);
   
   int i = 2;
   volatile int j = 0;
   while (1) {
     j ++;
     if (j == 10000) {
-      printf("Hello World from Navy-apps for the %dth time!\n", i ++);
+      printf("Eello World from Navy-apps for the %dth time!\n", i ++);
       j = 0;
     }
   }
