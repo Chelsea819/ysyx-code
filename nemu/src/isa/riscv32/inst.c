71962cb1f0cbd552eb71c92a375db9cde6a0b95b
tracer-ysyx <tracer@ysyx.org>
2024-03-12 22:11:52 +0800

    > compile NEMU ysyx_22041211 王晓雪 Linux chelsea-virtual-machine 5.19.0-35-generic #36~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Feb 17 15:17:25 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  22:11:52 up 3 days, 21:38,  5 users,  load average: 1.73, 1.13, 0.93

--- nemu/src/isa/riscv32/inst.c
+++ nemu/src/isa/riscv32/inst.c
@@ -148,7 +148,7 @@ int isa_exec_once(Decode *s) {
   s->isa.inst.val = inst_fetch(&s->snpc, 4);
   // int ret = decode_exec(s);
   // if((s->isa.inst.val & 0x0000007f)== 115) {printf("C(773) = 0x%08x\n",C(773));}
-  if(!(s->pc >= 0x80000ec4 && s->pc <= 0x80001550)) printf("s->pc = 0x%08x s->isa.inst.val = 0x%08x\n",s->pc,s->isa.inst.val);
+  // if(!(s->pc >= 0x80000ec4 && s->pc <= 0x80001550)) printf("s->pc = 0x%08x s->isa.inst.val = 0x%08x\n",s->pc,s->isa.inst.val);
   // if(s->pc == 0x800013d0) assert(0);
   return decode_exec(s);
 }
