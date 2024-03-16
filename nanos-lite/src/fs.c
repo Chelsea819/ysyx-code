8d92d35975896c9eef9687f2149d80b3e1a93bfb
tracer-ysyx <tracer@ysyx.org>
2024-03-16 21:43:29 +0800

    > compile NEMU ysyx_22041211 王晓雪 Linux chelsea-virtual-machine 5.19.0-35-generic #36~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Feb 17 15:17:25 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:43:29 up  3:36,  4 users,  load average: 1.95, 1.32, 0.98

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -106,6 +106,7 @@ size_t fs_write(int fd, const void *buf, size_t len){
 
 
 size_t fs_lseek(int fd, size_t offset, int whence){
+  printf("fd = %d\n",fd);
   assert(fd >= 0 && fd < sizeof(file_table) / sizeof(Finfo));
   int pre = file_table[fd].disk_offset;
   if(whence == SEEK_SET){
