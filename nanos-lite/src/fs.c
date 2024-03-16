4d83614d13e1e45028e50510f22c8970ce631c70
tracer-ysyx <tracer@ysyx.org>
2024-03-16 21:00:56 +0800

    > compile NEMU ysyx_22041211 王晓雪 Linux chelsea-virtual-machine 5.19.0-35-generic #36~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Feb 17 15:17:25 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:00:56 up  2:54,  4 users,  load average: 0.44, 0.81, 0.78

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -80,7 +80,7 @@ size_t fs_read(int fd, void *buf, size_t len){
 }
 
 size_t fs_write(int fd, const void *buf, size_t len){
-  printf("fs_write\n");
+  printf("fs_write fd = %d\n",fd);
   if(fd == 1 || fd == 2){  // stdout stderr
     int i = 0; 
     for(i = 0; i < len; i ++){
