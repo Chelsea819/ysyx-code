448d44a2c52fb11cc232813f77d2cd221e296628
tracer-ysyx <tracer@ysyx.org>
2024-03-17 12:39:41 +0800

    > compile NEMU ysyx_22041211 王晓雪 Linux chelsea-virtual-machine 5.19.0-35-generic #36~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Feb 17 15:17:25 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  12:39:41 up  7:05,  4 users,  load average: 1.74, 0.83, 0.77

--- navy-apps/tests/file-test/main.c
+++ navy-apps/tests/file-test/main.c
@@ -15,8 +15,8 @@ int main() {
     fscanf(fp, "%d", &n);
     assert(n == i + 1);
   }
-  size = ftell(fp);
-  printf("size = %ld\n",size);
+  // size = ftell(fp);
+  // printf("size = %ld\n",size);
   fseek(fp, 0, SEEK_SET);
   for (i = 0; i < 500; i ++) {
     fprintf(fp, "%4d\n", i + 1 + 1000);
