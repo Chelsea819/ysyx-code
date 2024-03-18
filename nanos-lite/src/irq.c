c21fec55f96650607013e31fe890c9d7210ab007
tracer-ysyx <tracer@ysyx.org>
2024-03-18 22:34:01 +0800

    > run NEMU ysyx_22041211 王晓雪 Linux chelsea-virtual-machine 5.19.0-35-generic #36~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Feb 17 15:17:25 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  22:34:01 up 18:23,  5 users,  load average: 1.55, 0.95, 0.80

--- nanos-lite/src/irq.c
+++ nanos-lite/src/irq.c
@@ -4,7 +4,7 @@ void do_syscall(Context *c);
 static Context* do_event(Event e, Context* c) {
   // printf("do_event : &c = 0x%08x\n",&c);
   printf("do_event : c = %p\n",c);
-  // printf("\n");
+  printf("1s\n");
   switch (e.event) {
     case EVENT_YIELD: printf("Yield!\n"); break;
     case EVENT_SYSCALL: do_syscall(c); printf("[do_event] c->mepc = 0x%08x\n",c->mepc);break;
