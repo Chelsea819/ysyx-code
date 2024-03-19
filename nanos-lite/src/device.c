b50640d63601f0f55eb318bdde6444712ac0c179
tracer-ysyx <tracer@ysyx.org>
2024-03-19 17:26:39 +0800

    > compile NEMU ysyx_22041211 王晓雪 Linux chelsea-virtual-machine 5.19.0-35-generic #36~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Feb 17 15:17:25 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:26:39 up 1 day, 5 min,  5 users,  load average: 0.71, 0.70, 0.54

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -28,12 +28,12 @@ size_t serial_write(const void *buf, size_t offset, size_t len) {
 size_t events_read(void *buf, size_t offset, size_t len) {
   int i = 0;
   AM_INPUT_KEYBRD_T ev = io_read(AM_INPUT_KEYBRD);
-  // printf("sizeof(keyname[ev.keycode] = %d\n",sizeof(keyname[ev.keycode]));
+  printf("sizeof(keyname[ev.keycode] = %d\n",strlen(keyname[ev.keycode]));
   for(i = 0; i < len && i < strlen(keyname[ev.keycode]); i ++){
     *((char *)buf + i) = keyname[io_read(AM_INPUT_KEYBRD).keycode][i];
   }
   *((char *)buf + i) = '\0';
-  // printf("buf = %s ev.keycode = %d ev.keydown = %d\n",buf ,ev.keycode,(ev.keydown ? 1 : 0));
+  printf("buf = %s ev.keycode = %d ev.keydown = %d\n",buf ,ev.keycode,(ev.keydown ? 1 : 0));
   
   return i;
 }
