d48c909e4db1ffa31ae16bff34aece7ddca542fe
tracer-ysyx <tracer@ysyx.org>
2024-03-19 15:19:39 +0800

    > compile NEMU ysyx_22041211 王晓雪 Linux chelsea-virtual-machine 5.19.0-35-generic #36~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Feb 17 15:17:25 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:19:39 up 22:03,  5 users,  load average: 1.57, 1.21, 1.12

--- navy-apps/tests/event-test/main.c
+++ navy-apps/tests/event-test/main.c
@@ -6,10 +6,9 @@ int main() {
   int time = 0;
   int n = 1;
   while (1) {
+    int i = 0;
+    while(i ++ < 50000);
     char buf[64];
-    do{
-            time = NDL_GetTicks();
-        }while(time / 500000 < n++);
     if (NDL_PollEvent(buf, sizeof(buf))) {
       printf("receive event: [%s]\n", buf);
     }
