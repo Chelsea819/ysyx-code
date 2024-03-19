2efcec92f04d6cb0e2e3538892bd40303185d480
tracer-ysyx <tracer@ysyx.org>
2024-03-19 16:06:26 +0800

    > compile NEMU ysyx_22041211 王晓雪 Linux chelsea-virtual-machine 5.19.0-35-generic #36~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Feb 17 15:17:25 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:06:26 up 22:50,  5 users,  load average: 0.74, 0.64, 0.59

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -32,6 +32,7 @@ size_t events_read(void *buf, size_t offset, size_t len) {
   for(i = 0; i < len && i < sizeof(keyname[ev.keycode]); i ++){
     *((char *)buf + i) = keyname[io_read(AM_INPUT_KEYBRD).keycode][i];
   }
+  *((char *)buf + i) = '\0';
   return i;
 }
 
