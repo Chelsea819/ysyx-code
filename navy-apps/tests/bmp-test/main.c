94885fc241b8dc551b32916d46cf0afc2158c784
tracer-ysyx <tracer@ysyx.org>
2024-03-20 21:10:47 +0800

    > compile NEMU ysyx_22041211 王晓雪 Linux chelsea-virtual-machine 5.19.0-35-generic #36~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Feb 17 15:17:25 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:10:47 up 1 day,  2:50,  5 users,  load average: 1.39, 1.04, 0.95

--- navy-apps/tests/bmp-test/main.c
+++ navy-apps/tests/bmp-test/main.c
@@ -7,9 +7,9 @@
 int main() {
   NDL_Init(0);
   int w, h;
-  void *bmp = BMP_Load("/share/pictures/projectn.bmp", &w, &h);
+  void *bmp = BMP_Load("/share/pictures/projectn.bmp", &w, &h);assert(0);
   assert(bmp);
-  assert(0);
+  
   NDL_OpenCanvas(&w, &h);
   NDL_DrawRect(bmp, 0, 0, w, h);
   free(bmp);
