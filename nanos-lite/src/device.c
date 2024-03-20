cdc7a9ba0f1fe7352f776a27d59d6423a58e7926
tracer-ysyx <tracer@ysyx.org>
2024-03-20 22:16:19 +0800

    > compile NEMU ysyx_22041211 王晓雪 Linux chelsea-virtual-machine 5.19.0-35-generic #36~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Feb 17 15:17:25 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  22:16:19 up 1 day,  3:55,  5 users,  load average: 1.41, 0.97, 0.63

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -15,6 +15,7 @@ static const char *keyname[256] __attribute__((used)) = {
 };
 
 size_t serial_write(const void *buf, size_t offset, size_t len) {
+  printf("serial_write\n");
   int i = 0; 
   printf("i = %d\n",i);
   for(i = 0; i < len; i ++){
