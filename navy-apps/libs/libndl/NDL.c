3b55fe4eb196f2479de60fd597cc473f27db6c97
tracer-ysyx <tracer@ysyx.org>
2024-03-21 21:24:09 +0800

    > compile NEMU ysyx_22041211 王晓雪 Linux chelsea-virtual-machine 5.19.0-35-generic #36~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Feb 17 15:17:25 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:24:09 up 1 day,  9:13,  5 users,  load average: 1.68, 0.76, 0.61

--- navy-apps/libs/libndl/NDL.c
+++ navy-apps/libs/libndl/NDL.c
@@ -83,7 +83,7 @@ void NDL_OpenCanvas(int *w, int *h) {
 // 向画布`(x, y)`坐标处绘制`w*h`的矩形图像, 并将该绘制区域同步到屏幕上
 // 图像像素按行优先方式存储在`pixels`中, 每个像素用32位整数以`00RRGGBB`的方式描述颜色
 void NDL_DrawRect(uint32_t *pixels, int x, int y, int w, int h) {
-  fseek(fb_fd, screen_w*y + x, SEEK_SET);
+  lseek(fb_fd, screen_w*y + x, SEEK_SET);
   write(fb_fd, pixels, w*h);
 }
 
