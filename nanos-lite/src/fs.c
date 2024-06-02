d4284190b8079c44da0c62f8968d1ccd9b096738
tracer-ysyx <tracer@ysyx.org>
2024-06-02 15:08:30 +0800

    > compile NEMU ysyx_22041211 王晓雪 Linux chelsea-virtual-machine 5.19.0-35-generic #36~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Feb 17 15:17:25 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:08:30 up 10:43,  4 users,  load average: 1.52, 1.52, 1.15

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -71,10 +71,10 @@ int fs_open(const char *pathname, int flags, int mode){
     init_file_offset();
   // find the certain file
   int file_num = sizeof(file_table) / sizeof(Finfo);
-  // printf("pathname = %s file_num = %d\n",pathname,file_num);
+  printf("pathname = %s file_num = %d\n",pathname,file_num);
   int fd = 0;
   for(fd = 0; fd < file_num; fd ++){
-    // printf("file_table[%d].name = %s\n",fd,file_table[fd].name);
+    printf("file_table[%d].name = %s\n",fd,file_table[fd].name);
     if(strcmp(file_table[fd].name, pathname) == 0){
       break;
     }
