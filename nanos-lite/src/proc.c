dcf6e47da49d54cdea2dabc9152d7c8758bac755
tracer-ysyx <tracer@ysyx.org>
2024-06-04 15:06:57 +0800

    > compile NEMU ysyx_22041211 王晓雪 Linux chelsea-virtual-machine 5.19.0-35-generic #36~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Feb 17 15:17:25 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:06:57 up 12:54,  1 user,  load average: 1.66, 1.35, 0.97

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -12,6 +12,10 @@ void switch_boot_pcb() {
   current = &pcb_boot;
 }
 
+void context_kload(PCB *pcb, void (*entry)(void *), void *arg){
+  pcb->cp = kcontext((Area) { pcb->stack, pcb + 1 }, entry, arg);
+}
+
 void hello_fun(void *arg) {
   int j = 1;
   while (1) {
@@ -22,14 +26,19 @@ void hello_fun(void *arg) {
 }
 
 void init_proc() {
-  switch_boot_pcb();
-
   Log("Initializing processes...");
 
+  context_kload(&pcb[0], hello_fun, "A");
+  context_kload(&pcb[1], hello_fun, "B");
+
+  switch_boot_pcb();
+
   // load program here
-  naive_uload(NULL, "/bin/nslider");
+  // naive_uload(current, "/bin/nslider");
 }
 
-Context* schedule(Context *prev) {
-  return NULL;
+Context* schedule(Event ev, Context *prev) {
+  current->cp = prev;
+  current = (current == &pcb[0] ? &pcb[1] : &pcb[0]);
+  return current->cp;
 }
