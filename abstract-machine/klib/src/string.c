17934ce5e4c2d9866bc039d7042f82b219ba2036
tracer-ysyx <tracer@ysyx.org>
2024-08-01 15:19:21 +0800

    > compile NEMU ysyx_22041211 王晓雪 Linux chelsea-virtual-machine 5.19.0-35-generic #36~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Feb 17 15:17:25 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:19:21 up 1 day,  3:36,  1 user,  load average: 1.90, 1.70, 1.49

--- abstract-machine/klib/src/string.c
+++ abstract-machine/klib/src/string.c
@@ -84,7 +84,6 @@ int strncmp(const char *s1, const char *s2, size_t n) {
   }
   if(s1[i] == s2[i]) return 0;
   if(s1[i] == '\0') return -1;
-  // if(s2[i] == '\0') return 1;
   return 1;
   //panic("Not implemented");
 }
