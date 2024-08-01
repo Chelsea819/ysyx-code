2a607b3557a1f535120d0ea6201ee6d49c826091
tracer-ysyx <tracer@ysyx.org>
2024-08-01 09:50:44 +0800

    > compile NEMU ysyx_22041211 王晓雪 Linux chelsea-virtual-machine 5.19.0-35-generic #36~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Feb 17 15:17:25 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  09:50:44 up 23:23,  1 user,  load average: 1.31, 0.87, 0.74

--- abstract-machine/klib/src/stdlib.c
+++ abstract-machine/klib/src/stdlib.c
@@ -1,19 +1,15 @@
 #include <am.h>
 #include <klib.h>
 #include <klib-macros.h>
-#include <stdio.h>
+// #include <tlsf.h>
 
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 static unsigned long int next = 1;
 
-#if !(defined(__ISA_NATIVE__) && defined(__NATIVE_USE_KLIB__))
-  static char * addr = NULL;
-#endif
-
 int rand(void) {
   // RAND_MAX assumed to be 32767
   next = next * 1103515245 + 12345;
-  return (unsigned int)(next/65536) % 32768;
+  return (unsigned int)(next / 65536) % 32768;
 }
 
 void srand(unsigned int seed) {
@@ -26,35 +22,35 @@ int abs(int x) {
 
 int atoi(const char* nptr) {
   int x = 0;
-  while (*nptr == ' ') { nptr ++; }
+  while (*nptr == ' ') { nptr++; }
   while (*nptr >= '0' && *nptr <= '9') {
     x = x * 10 + *nptr - '0';
-    nptr ++;
+    nptr++;
   }
   return x;
 }
 
-void *malloc(size_t size) {
-  // On native, malloc() will be called during initializaion of C runtime.
-  // Therefore do not call panic() here, else it will yield a dead recursion:
-  //   panic() -> putchar() -> (glibc) -> malloc() -> panic()
-#if !(defined(__ISA_NATIVE__) && defined(__NATIVE_USE_KLIB__))
-  // if(!size) return NULL;
-  if(addr == NULL){
-    addr = (char *)heap.start;
+
+char* head_start_p = NULL;
+void* malloc(size_t size) {
+  if (head_start_p == NULL) {
+    head_start_p = heap.start;
   }
-  void *hbrk = addr;
-  addr = addr + size;
-  // assert(size >= 0);
-  // assert((uintptr_t)heap.start > (uintptr_t)addr);
-  assert((uintptr_t)heap.start <= (uintptr_t)addr);
-  assert((uintptr_t)heap.start <= (uintptr_t)addr && (uintptr_t)addr < (uintptr_t)heap.end);
-  return hbrk;
-#endif
-  return NULL;
+  // static int i = 0;
+  // if (i == 0) {
+  //   i++;
+  //   printf("malloc init\n");
+  //   init_memory_pool(heap.end - heap.start + 1, heap.start);
+  // }
+  // printf("malloc\n");
+  // return tlsf_malloc(size);
+  char* last_p = head_start_p;
+  head_start_p += size;
+  return last_p;
 }
 
-void free(void *ptr) {
+void free(void* ptr) {
+  // tlsf_free(ptr);
 }
 
 #endif
