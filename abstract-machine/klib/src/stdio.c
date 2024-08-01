bca5aebf6a9ae0f3b4e7c2ee173520270d81c8e7
tracer-ysyx <tracer@ysyx.org>
2024-08-01 14:36:00 +0800

    > compile NEMU ysyx_22041211 王晓雪 Linux chelsea-virtual-machine 5.19.0-35-generic #36~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Feb 17 15:17:25 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  14:36:00 up 1 day,  2:52,  1 user,  load average: 3.12, 2.12, 1.29

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -1,123 +1,255 @@
-/*
- * @Author       : 中北大学-聂怀昊
- * @Date         : 2024-05-26 21:57:18
- * @LastEditTime : 2024-07-16 17:45:09
- * @FilePath     : /ysyx/ysyx-workbench/abstract-machine/klib/src/stdio.c
- * @Description  :
- *
- * Copyright (c) 2024 by 873040830@qq.com, All Rights Reserved.
- */
 #include <am.h>
 #include <klib.h>
 #include <klib-macros.h>
 #include <stdarg.h>
 
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
+#define MAX_SIZE 1024*4
+#define NUM_BUF 50
+// -2147483648
+void convert(int num,int* numAdd,char *arr_tmp,int flag){
+  int tmp = num % flag; //-8
+  if(tmp < 0){tmp *= -1;}
+  arr_tmp[(*numAdd)] = tmp + 48; 
+  // putch(arr_tmp[(*numAdd)]);
+  // putch('\n');
+  (*numAdd) --;
+}
 
-char buf[1024];
-void putch(char ch);
+void convert_u(unsigned long num,int* numAdd,char *arr_tmp,int flag){
+  unsigned long tmp = num % flag; //-8
+  if(tmp >= 0 && tmp <= 9)
+    arr_tmp[(*numAdd)] = tmp + 48; 
+  else
+    arr_tmp[(*numAdd)] = tmp + 'a' - 10; 
+  // putch(arr_tmp[(*numAdd)]);
+  // putch('\n');
+  (*numAdd) --;
+}
 
-int printf(const char *fmt, ...)
-{
-  va_list ap;
-  va_start(ap, fmt);
+void intHandel(int *neg, int *num, int *numAdd, int flag, char *arr_tmp){
+  //负数
+  if(*num < 0){
+    *neg = 1;
+  }  
+  do{
+    //判断这个负数是否可以直接转换为正数
+    if(*num < 0 && *num >= -2147483647) *num *= -1;
+    //将整型数字转换成字符串类型
+    convert(*num, numAdd, arr_tmp,flag);
+    *num /= flag;
+  }while(*num != 0);   
+  (*numAdd) ++;
+}
 
-  int val = vsnprintf(buf, 1024, fmt, ap);
-  char *tmp = buf;
-  while (*tmp != 0)
-  {
-    putch(*tmp);
-    tmp++;
+void u_intHandel(unsigned long *num, int *numAdd,unsigned long flag, char *arr_tmp){
+  do{
+    //将整型数字转换成字符串类型
+    convert_u(*num, numAdd, arr_tmp,flag);
+    *num /= flag;
+  }while(*num != 0);   
+  (*numAdd) ++;
+}
+
+void formatHandel(bool *if_wid, bool *if_for, int numAdd, int *width, char *out, int *k){
+  if(*if_wid && NUM_BUF - numAdd - 1 < *width){
+    for(int i = *width - (NUM_BUF - numAdd - 1); i > 0 ; i --, (*k) ++)
+      if(*if_for) out[*k] = '0';
+      else out[*k] = ' ';
+    *if_for = false;
+    *if_wid = false;
+    *width = 0;
   }
-
-  va_end(ap);
-  return val;
 }
 
-int vsprintf(char *out, const char *fmt, va_list ap)
-{
-  panic("Not implemented");
-}
 
-int sprintf(char *out, const char *fmt, ...)
-{
-  va_list ap;
-  va_start(ap, fmt);
+int vsnprintf(char *out, size_t n, const char *fmt, va_list ap) {
+  // panic("Not implemented");
+  int percent = 0; //检测%
+  // int tmp = 0;     //存放%的下标
+  int k = 0;       //out数组的下标
+  int flag = 0;
+  int width = 0;
+  int neg = 0;
+  bool if_wid = false;
+  bool if_for = false;
+  bool if_long = false;
 
-  int val = vsnprintf(out, 1024, fmt, ap);
-  va_end(ap);
+  for(int i = 0; k < n-1 && *(fmt + i) != '\0'; i++){
+    // if(fmt[i] == '%') {putch(fmt[i]);percent ^= 1; tmp = i;}
+    if(fmt[i] == '%') {percent ^= 1; }
 
-  return val;
-}
-
-int snprintf(char *out, size_t n, const char *fmt, ...)
-{
-  panic("Not implemented");
-}
-
-int vsnprintf(char *out, size_t n, const char *fmt, va_list ap)
-{
-  char *start = out;
-  while (n-- && *fmt != '\0')
-  {
-    if (*fmt == '%')
-    {
-      fmt++;
-      if (*fmt == 's')
-      {
-        char *tmp_s = va_arg(ap, char *);
-        while (*tmp_s != '\0')
-        {
-          *out++ = *tmp_s++;
+    //当percent为1时进入循环,即出现奇数个`%`
+    //匹配到`%`后面的格式化输出标识符
+    else if(percent == 1){
+      // putch(fmt[i]);
+      //%s
+      if(fmt[i] == 's'){
+        percent = 0;
+        char *str = va_arg(ap,char*);
+        for(int j = 0; k < n-1 && str[j] != '\0'; j++,k++){
+          // putch(fmt[i]);
+          out[k] = str[j];
         }
       }
-      else if (*fmt == 'd')
-      {
-        int tmp_int = va_arg(ap, int);
-        if (tmp_int < 0)
-        {
-          *out++ = '-';
-          tmp_int = -1 * tmp_int;
+
+      // %c
+      else if(fmt[i] == 'c'){
+        // putch(fmt[i]);
+        percent = 0;
+        char c = va_arg(ap,int);
+        out[k] = c;
+        k ++;
+      }
+      else if(fmt[i] == 'p'){
+        percent = 0;
+        char arr_tmp[NUM_BUF] = {0};  //存放数字转换成的字符
+        int numAdd = NUM_BUF - 2;  //数组的下标 从后往前存
+        if_for = true;
+        if_wid = true;
+
+        unsigned long num = (unsigned long)va_arg(ap, void*); //要替换的数字
+        //每一次调用va_arg()都会修改ap，这样下一次调用 就会返回下一个参数
+        //va_arg(ap,type) 这个type是为了初始化一个指向目标的指针
+        //如果type不匹配或者没有下一个参数，会出现随机错误
+
+        flag = 16; 
+        neg = 0;
+
+        u_intHandel(&num, &numAdd, flag, arr_tmp);
+        out[k ++] = '0';
+        out[k ++] = 'x';
+        formatHandel(&if_wid, &if_for, numAdd, &width, out, &k);
+
+        //将数字存入out数组
+        for( ; k < n-1 && numAdd < NUM_BUF - 1 ; k++,numAdd ++){     
+          out[k] = arr_tmp[numAdd];
         }
-        int number = tmp_int;
-        int len = 0;
-        do
-        {
-          number /= 10;
-          len++;
-        } while (number);
-        out = out + len - 1;
-        int tmp_len = len;
-        while (tmp_len--)
-        {
-          int tmp = tmp_int % 10;
-          *out-- = tmp + 48;
-          tmp_int /= 10;
+        if(if_long) if_long = false;
+      }
+      else if(fmt[i] == '0' && !if_for && !if_wid){
+        // putch('f');
+        if_for = true;
+      }
+      else if(fmt[i] >= '0' && fmt[i] <= '9'){
+        // putch('w');
+        if(!if_wid)
+          width = fmt[i] - '0';
+        else
+          width = width * 10 + (fmt[i] - '0');
+        if_wid = true;
+      }
+      else if(if_for && !if_wid)
+        panic("Invalid format");
+      else if(fmt[i] == 'l'){
+        if_long = true;
+      }
+      // %d
+      else if(fmt[i] == 'd'){
+        // putch(fmt[i]);
+        percent = 0;
+        char arr_tmp[NUM_BUF] = {0};  //存放数字转换成的字符
+        int numAdd = NUM_BUF - 2;  //数组的下标 从后往前存
+
+        int num = va_arg(ap,int); //要替换的数字
+        //每一次调用va_arg()都会修改ap，这样下一次调用 就会返回下一个参数
+        //va_arg(ap,type) 这个type是为了初始化一个指向目标的指针
+        //如果type不匹配或者没有下一个参数，会出现随机错误
+        flag = 10; 
+        neg = 0;
+        intHandel(&neg, &num, &numAdd, flag, arr_tmp);
+        formatHandel(&if_wid, &if_for, numAdd, &width, out, &k);
+        //将数字存入out数组
+        for( ; k < n-1 && numAdd < NUM_BUF - 1 ; k++,numAdd ++){
+          //负数      
+          if(neg) {out[k] = '-'; neg = 0;}
+          else {out[k] = arr_tmp[numAdd]; }
         }
-        out += (len + 1);
+        if(if_long) if_long = false;
+      } 
+
+      else if(fmt[i] == 'x'){
+        percent = 0;
+        char arr_tmp[NUM_BUF] = {0};  //存放数字转换成的字符
+        int numAdd = NUM_BUF - 2;  //数组的下标 从后往前存
+
+        unsigned long num = va_arg(ap,unsigned long); //要替换的数字
+        //每一次调用va_arg()都会修改ap，这样下一次调用 就会返回下一个参数
+        //va_arg(ap,type) 这个type是为了初始化一个指向目标的指针
+        //如果type不匹配或者没有下一个参数，会出现随机错误
+
+        flag = 16; 
+        neg = 0;
+
+        u_intHandel(&num, &numAdd, flag, arr_tmp);
+        formatHandel(&if_wid, &if_for, numAdd, &width, out, &k);
+
+        //将数字存入out数组
+        for( ; k < n-1 && numAdd < NUM_BUF - 1 ; k++,numAdd ++){     
+          out[k] = arr_tmp[numAdd];
+        }
+        if(if_long) if_long = false;
       }
-      else if (*fmt == '%')
-      {
-        *out++ = '%';
-      }
-      else if (*fmt == 'c')
-      {
-        char tmp_char = va_arg(ap, int);
-        *out++ = tmp_char;
-      }
-      else
-      {
+      else {
+        putch(fmt[i]);
+        panic("Not completed format");
         return -1;
       }
+      
     }
-    else
-    {
-      *out++ = *fmt;
-    }
-    fmt++;
+    else {
+      // putch(fmt[i]);
+      out[k] = fmt[i];
+      k++;
+    } 
   }
-  *out = '\0';
-  return out - start;
+  out[k] = '\0';
+  va_end(ap);
+  return k;
 }
+
+int printf(const char *fmt, ...) {
+  va_list ap;
+  va_start(ap,fmt); //初始化ap
+  char out[MAX_SIZE];
+  int ret = vsprintf(out, fmt, ap);
+  assert(ret <= MAX_SIZE && ret >= 0);
+  for(int i = 0; i <= ret; i ++){
+    putch(out[i]);
+  }
+  va_end(ap);
+  return ret;
+  // panic("Not implemented");
+}
+
+int vsprintf(char *out, const char *fmt, va_list ap) {
+  // panic("Not implemented");
+  int ret = vsnprintf(out, MAX_SIZE, fmt, ap);
+  return ret;
+}
+
+//碰到%后，指示%的指针不变 pCurrent先前跑 找到格式化输出的格式
+
+int sprintf(char *out, const char *fmt, ...) {
+  va_list ap;
+  va_start(ap,fmt); //初始化ap
+
+  int ret = vsprintf(out, fmt, ap);
+
+  va_end(ap);
+  return ret;
+}
+
+int snprintf(char *out, size_t n, const char *fmt, ...) {
+  // panic("Not implemented");
+  va_list ap;
+  va_start(ap,fmt); //初始化ap
+
+  int ret = vsnprintf(out, n, fmt, ap);
+
+  va_end(ap);
+  return ret;
+}
+
 
 #endif
--- abstract-machine/scripts/platform/nemu.mk
+++ abstract-machine/scripts/platform/nemu.mk
@@ -6,13 +6,13 @@ AM_SRCS := platform/nemu/trm.c \
            platform/nemu/ioe/audio.c \
            platform/nemu/ioe/disk.c \
            platform/nemu/mpe.c
-
+ALL ?= $(NAME)
 CFLAGS    += -fdata-sections -ffunction-sections
 LDFLAGS   += -T $(AM_HOME)/scripts/linker.ld \
              --defsym=_pmem_start=0x80000000 --defsym=_entry_offset=0x0
 LDFLAGS   += --gc-sections -e _start
-NEMUFLAGS += -l $(shell dirname $(IMAGE).elf)/nemu-log.txt -b
-NEMUFLAGS += -f $(IMAGE).elf
+NEMUFLAGS += -l $(shell dirname $(IMAGE).elf)/nemu-log.txt 
+NEMUFLAGS += -f $(shell dirname $(IMAGE).elf)/$(ALL)-$(ARCH).elf -f $(NEMU_HOME)/../navy-apps/apps/nslider/build/nslider-riscv32 #-b
 
 CFLAGS += -DMAINARGS=\"$(mainargs)\"
 CFLAGS += -I$(AM_HOME)/am/src/platform/nemu/include
