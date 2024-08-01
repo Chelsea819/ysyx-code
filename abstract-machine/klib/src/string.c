1e63f6f39c363c40b393c50a58c158d2d7ab752c
tracer-ysyx <tracer@ysyx.org>
2024-08-01 14:50:14 +0800

    > compile NEMU ysyx_22041211 王晓雪 Linux chelsea-virtual-machine 5.19.0-35-generic #36~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Feb 17 15:17:25 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  14:50:14 up 1 day,  3:07,  1 user,  load average: 1.02, 1.06, 1.18

--- abstract-machine/klib/src/string.c
+++ abstract-machine/klib/src/string.c
@@ -1,136 +1,150 @@
-/*
- *
- * Copyright (c) 2024 by 873040830@qq.com, All Rights Reserved.
- */
 #include <klib.h>
 #include <klib-macros.h>
 #include <stdint.h>
+// #include <stdio.h>
 
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
-size_t strlen(const char *s)
-{
-  size_t len = 0;
-  while (s[len] != '\0')
-  {
-    ++len;
+/*calculates  the length of the string 
+ pointed to by s, excluding 
+ the terminating null byte ('\0').*/
+/*returns the number of bytes in
+       the string pointed to by s.*/
+size_t strlen(const char *s) {
+  size_t length = 0;
+  for(int i = 0; s[i] != '\0'; i++){
+    length ++;
   }
-  return len;
+  return length;
+  //panic("Not implemented");
 }
 
-char *strcpy(char *dst, const char *src)
-{
-  size_t i;
-  for (i = 0; src[i] != '\0'; ++i)
-  {
+/*return a pointer
+       to the destination string dest.*/
+/*copies the string pointed to by
+       src, including the terminating null  byte  ('\0'),  to
+       the  buffer  pointed  to by dest.*/
+
+char *strcpy(char *dst, const char *src) {
+  // assert(strlen(src) > strlen(dst));
+  int i = 0;
+  for(i = 0; src[i] != '\0'; i++){
     dst[i] = src[i];
   }
-  dst[i] = src[i];
+  dst[i] = '\0';
   return dst;
+  //panic("Not implemented");
 }
 
-char *strncpy(char *dst, const char *src, size_t n)
-{
-  size_t i;
-  for (i = 0; i < n && src[i] != '\0'; ++i)
-  {
+char *strncpy(char *dst, const char *src, size_t n) {
+  int i = 0;
+  for(i = 0; i < n && src[i] != '\0' ; i ++){
     dst[i] = src[i];
   }
-  for (; i < n; ++i)
-  {
+  for(; i < n; i++){
     dst[i] = '\0';
   }
+   return dst;
+  //panic("Not implemented");
+}
+
+char *strcat(char *dst, const char *src) {
+  int i = 0;
+  int k = 0;
+  for(i = 0; dst[i] != '\0'; i++);
+  for(; src[k] != '\0'; i++ ,k++){
+    dst[i] = src[k];
+  }
+  dst[i] = '\0';
   return dst;
+  //panic("Not implemented");
 }
 
-char *strcat(char *dst, const char *src)
-{
-  size_t dst_len = strlen(dst), i;
-  for (i = 0; src[i] != '\0'; ++i)
-  {
-    dst[dst_len + i] = src[i];
+int strcmp(const char *s1, const char *s2) {
+  int i = 0;
+  for(i = 0; s1[i] != '\0'&& s2[i] != '\0' ; i ++){
+    if((unsigned char)s1[i] == (unsigned char)s2[i]) continue;
+    if((unsigned char)s1[i] > (unsigned char)s2[i]) return 1;
+    if((unsigned char)s1[i] < (unsigned char)s2[i]) return -1;
   }
-  dst[dst_len + i] = '\0';
-  return dst;
+  size_t len1 = strlen(s1);
+  size_t len2 = strlen(s2);
+  if(len1 > len2) return 1;
+  else if(len1 < len2) return -1;
+  return 0;
+  //panic("Not implemented");
 }
 
-int strcmp(const char *s1, const char *s2)
-{
-  size_t i = 0;
-  for (; s1[i] != '\0' && s2[i] != '\0'; ++i)
-  {
-    if (s1[i] != s2[i])
-      return s1[i] - s2[i];
+int strncmp(const char *s1, const char *s2, size_t n) {
+  int i = 0;
+  for(i = 0; s1[i] != '\0'&& s2[i] != '\0' && i < n; i ++){
+    if((unsigned char)s1[i] == (unsigned char)s2[i]) continue;
+    if((unsigned char)s1[i] > (unsigned char)s2[i]) return 1;
+    if((unsigned char)s1[i] < (unsigned char)s2[i]) return -1;
   }
-  return s1[i] - s2[i];
+  if(s1[i] == '\0') return -1;
+  if(s2[i] == '\0') return 1;
+  return 0;
+  //panic("Not implemented");
 }
 
-int strncmp(const char *s1, const char *s2, size_t n)
-{
-  size_t i = 0;
-  for (; s1[i] != '\0' && s2[i] != '\0' && i < n; ++i)
-  {
-    if (s1[i] != s2[i])
-      return s1[i] - s2[i];
-  }
-  if (i == n)
-    return 0;
-  return s1[i] - s2[i];
-}
-
-void *memset(void *s, int c, size_t n)
-{
-  char *ptr = s;
-  while (n--)
-  {
-    *ptr++ = c;
+/* fill memory with a constant byte */
+/* The memset() function returns a pointer 
+      to the memory area s. */
+void *memset(void *s, int c, size_t n) {
+  unsigned char *sp = (unsigned char *)s;
+  unsigned char val = (unsigned char)c;
+  for(size_t i = 0; i < n; i++){
+    *(sp + i) = val;
+    // putch('.');
+    // if(i == 35000) panic("Not implemented");
   }
   return s;
+  //panic("Not implemented");
 }
 
-void *memmove(void *dst, const void *src, size_t n)
-{
-  char *ptrd = dst;
-  const char *ptrs = src;
-  if (dst < src)
-  {
-    while (n--)
-    {
-      *ptrd++ = *ptrs++;
-    }
-  }
-  else
-  {
-    while (n--)
-    {
-      *(ptrd + n) = *(ptrs + n);
-    }
+/* The  memmove() function copies n bytes from memory area src to memory area dest.
+    The memory areas may overlap: copying takes place as though the bytes in src are
+    first  copied  into a temporary array that does not overlap src or dest, and the
+    bytes are then copied from the temporary array to dest. */
+
+void *memmove(void *dst, const void *src, size_t n) {
+  unsigned char *in = (unsigned char*)src;
+  unsigned char *out = (unsigned char*)dst;
+  for(int k = 0; k < n; k ++){
+    out[k] = in[k];
+  //panic("Not implemented");
   }
   return dst;
 }
 
-void *memcpy(void *out, const void *in, size_t n)
-{
-  char *ptrd = out;
-  const char *ptrs = in;
-  while (n--)
-  {
-    *ptrd++ = *ptrs++;
+//根据两个区间之间的关系，选择合适的copy方式
+//区间关系不同，copy方向不同
+void *memcpy(void *out, const void *in, size_t n) {
+  unsigned char *dst = (unsigned char *)out;
+  unsigned char *src = (unsigned char *)in;
+  if(src <= dst && (src + n) >= dst){
+    for(int k = n-1; k >= 0; k --){
+      dst[k] = src[k];
+    }
+  }
+  else{
+    for(int k = 0; k < n; k ++){
+      dst[k] = src[k];
+    }
   }
   return out;
+  //panic("Not implemented");
 }
 
-int memcmp(const void *s1, const void *s2, size_t n)
-{
-  const unsigned char *ptra = s1, *ptrb = s2;
-  int ret;
-  while (n--)
-  {
-    ret = *ptra - *ptrb;
-    if (ret != 0)
-      return ret;
+int memcmp(const void *s1, const void *s2, size_t n) {
+  for(int i = 0; i < n ; i ++){
+    if(*((unsigned char *)s1 + i) == *((unsigned char *)s2 + i)) continue;
+    if(*((unsigned char *)s1 + i) > *((unsigned char *)s2 + i)) return 1;
+    if(*((unsigned char *)s1 + i) < *((unsigned char *)s2 + i)) return -1;
   }
   return 0;
+  //panic("Not implemented");
 }
 
 #endif
