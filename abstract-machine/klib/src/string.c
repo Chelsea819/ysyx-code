87b4a84479d1156952525fd307218796502d4207
tracer-ysyx <tracer@ysyx.org>
2024-08-01 14:52:21 +0800

    > compile NEMU ysyx_22041211 王晓雪 Linux chelsea-virtual-machine 5.19.0-35-generic #36~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Feb 17 15:17:25 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  14:52:21 up 1 day,  3:09,  1 user,  load average: 2.22, 1.51, 1.33

--- abstract-machine/klib/src/string.c
+++ abstract-machine/klib/src/string.c
@@ -91,60 +91,59 @@ int strncmp(const char *s1, const char *s2, size_t n) {
 /* fill memory with a constant byte */
 /* The memset() function returns a pointer 
       to the memory area s. */
-void *memset(void *s, int c, size_t n) {
-  unsigned char *sp = (unsigned char *)s;
-  unsigned char val = (unsigned char)c;
-  for(size_t i = 0; i < n; i++){
-    *(sp + i) = val;
-    // putch('.');
-    // if(i == 35000) panic("Not implemented");
+void *memset(void *s, int c, size_t n)
+{
+  char *ptr = s;
+  while (n--)
+  {
+    *ptr++ = c;
   }
   return s;
-  //panic("Not implemented");
 }
 
-/* The  memmove() function copies n bytes from memory area src to memory area dest.
-    The memory areas may overlap: copying takes place as though the bytes in src are
-    first  copied  into a temporary array that does not overlap src or dest, and the
-    bytes are then copied from the temporary array to dest. */
-
-void *memmove(void *dst, const void *src, size_t n) {
-  unsigned char *in = (unsigned char*)src;
-  unsigned char *out = (unsigned char*)dst;
-  for(int k = 0; k < n; k ++){
-    out[k] = in[k];
-  //panic("Not implemented");
+void *memmove(void *dst, const void *src, size_t n)
+{
+  char *ptrd = dst;
+  const char *ptrs = src;
+  if (dst < src)
+  {
+    while (n--)
+    {
+      *ptrd++ = *ptrs++;
+    }
+  }
+  else
+  {
+    while (n--)
+    {
+      *(ptrd + n) = *(ptrs + n);
+    }
   }
   return dst;
 }
 
-//根据两个区间之间的关系，选择合适的copy方式
-//区间关系不同，copy方向不同
-void *memcpy(void *out, const void *in, size_t n) {
-  unsigned char *dst = (unsigned char *)out;
-  unsigned char *src = (unsigned char *)in;
-  if(src <= dst && (src + n) >= dst){
-    for(int k = n-1; k >= 0; k --){
-      dst[k] = src[k];
-    }
-  }
-  else{
-    for(int k = 0; k < n; k ++){
-      dst[k] = src[k];
-    }
+void *memcpy(void *out, const void *in, size_t n)
+{
+  char *ptrd = out;
+  const char *ptrs = in;
+  while (n--)
+  {
+    *ptrd++ = *ptrs++;
   }
   return out;
-  //panic("Not implemented");
 }
 
-int memcmp(const void *s1, const void *s2, size_t n) {
-  for(int i = 0; i < n ; i ++){
-    if(*((unsigned char *)s1 + i) == *((unsigned char *)s2 + i)) continue;
-    if(*((unsigned char *)s1 + i) > *((unsigned char *)s2 + i)) return 1;
-    if(*((unsigned char *)s1 + i) < *((unsigned char *)s2 + i)) return -1;
+int memcmp(const void *s1, const void *s2, size_t n)
+{
+  const unsigned char *ptra = s1, *ptrb = s2;
+  int ret;
+  while (n--)
+  {
+    ret = *ptra - *ptrb;
+    if (ret != 0)
+      return ret;
   }
   return 0;
-  //panic("Not implemented");
 }
 
 #endif
