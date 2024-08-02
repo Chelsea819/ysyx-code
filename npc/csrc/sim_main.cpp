128dbcad59929a426f3a58282f885dd43faf619a
tracer-ysyx <tracer@ysyx.org>
2024-08-02 11:45:31 +0800

    > sim RTL ysyx_22041211 王晓雪 Linux chelsea-virtual-machine 5.19.0-35-generic #36~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Feb 17 15:17:25 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  11:45:31 up  2:14,  1 user,  load average: 20.65, 8.00, 5.71

--- npc/csrc/sim_main.cpp
+++ npc/csrc/sim_main.cpp
@@ -38,7 +38,8 @@ void init_disasm(const char *triple);
 void init_difftest(char *ref_so_file, long img_size, int port);
 #endif
 void difftest_step(vaddr_t pc, vaddr_t npc);
-
+void init_sdb();
+void sdb_mainloop();
 NPCState npc_state = { .state = NPC_STOP };
 
 static char *img_file = NULL;
@@ -82,6 +83,7 @@ vluint64_t sim_time = 0;
 static uint8_t pmem[CONFIG_MSIZE] PG_ALIGN = {};
 
 TOP_NAME dut;
+extern WP *head;
 
 #ifdef CONFIG_WAVE
 VerilatedVcdC *m_trace = new VerilatedVcdC;
@@ -91,12 +93,6 @@ uint32_t convert_16(char *args);
 uint32_t convert_ten(char *args);
 static void watchPoints_display();
 int init_ftrace(const char *ftrace_file);
-
-#define NR_WP 32
-
-static WP wp_pool[NR_WP] = {};
-WP *head = NULL, *free_ = NULL;
-
 
 /* ------------------------------------timer------------------------------------ */
 
@@ -268,7 +264,7 @@ static void pmem_write_npc(paddr_t addr, int len, word_t data) {
 }
 
 
-static vaddr_t paddr_read(paddr_t addr,int len) {
+vaddr_t paddr_read(paddr_t addr,int len) {
   
 	if (likely(in_pmem(addr))) {
     word_t rdata = pmem_read_npc(addr,len);
@@ -452,17 +448,6 @@ void init_regex() {
       assert(0);
     }
   }
-}
-void init_wp_pool();
-
-void init_sdb()
-{
-  /* Compile the regular expressions. */
-  // 编译正则表达式
-  init_regex();
-
-  /* Initialize the watchpoint pool. */
-  init_wp_pool();
 }
 
 void sdb_set_batch_mode();
@@ -891,430 +876,11 @@ word_t expr(char *e, bool *success) {
   return eval(0, i - 1);
 }
 
-/* ------------------------------------watchpoint.c------------------------------------ */
-//static int gap = 31;
-
-WP* new_wp(char *args){
-  //search if existed the same
-  bool success = true;
-  WP* p_searchExist = head;
-  while(p_searchExist != NULL){
-    if(strcmp(p_searchExist->target,args) == 0) {
-      return NULL;
-    }
-    p_searchExist = p_searchExist->next;
-  }
-
-  //full
-  if(free_==NULL)  Assert(0,"wp_pool full!\n");
-
-  //find ava point
-  WP* get_wp = free_;
-  free_ = free_->next;
-  // while(get_wp != NULL){
-  //   if(get_wp->next == NULL){
-  //     break;
-  //   }
-  //   get_wp = get_wp->next;
-  // }
-
-  //cut it from free_
-  // get_wp->past->next = NULL;
-  get_wp->times = 0;
-  get_wp->target = (char *)malloc(strlen(args)+1);
-  strcpy(get_wp->target,args);
-  get_wp->data = expr(args,&success);
-  Assert(success,"Make_token fail!");
-
-  //add it to head list
-  if(head == NULL){
-    head = get_wp;
-    get_wp->NO = 0;
-    get_wp->next = NULL;
-    get_wp->past = NULL;
-    printf("the passing head address:%p\n",head);
-  }
-  else{
-    WP* addSpot = head;
-    while(addSpot->next != NULL){
-      addSpot = addSpot->next;
-    }
-    addSpot->next = get_wp;
-    get_wp->past = addSpot;
-    get_wp->NO = addSpot->NO + 1;
-    get_wp->next = NULL;
-  }
-
-    WP *index = head;
-    while(index){
-      printf("\n head \nNO:%d  target:%s   %p   data: %x\n",index->NO,index->target,index->target,index->data);
-      index = index->next;
-    }
-    //printf("head : %p\n",head);
-  //printf("head : %p\n",head->next);
-  return get_wp;
-}
-
-void free_wp(WP *wp){
-  if(!wp) Assert(0,"Free_wp received NULL!\n");
-
-  //remove it from head
-  if(wp->next == NULL && wp->past == NULL) head = NULL;
-  else{
-    if(wp->past != NULL) wp->past->next = wp->next;
-    else head = wp->next;
-    if(wp->next != NULL) wp->next->past = wp->past;
-    //printf("Remove it from %d in head\n",wp->NO);
-  }
-
-  free(wp->target);
-
-  wp->data = 0;
-  wp->times = 0;
-
-  //add it to free_
-  if(!free_) {
-    free_ = wp;
-    wp->next = NULL;
-    wp->NO = 0;
-    wp->past = NULL;
-    return;
-  }
-  
-  WP *index = free_;
-  while(index != NULL){
-    if(index->next == NULL){
-      index->next = wp;
-      wp->past = index;
-      wp->next = NULL;
-      wp->NO = index->NO + 1;
-      break;
-    }
-    index = index->next;
-  }
-  //printf("ADD it to %d in free_",wp->NO);
-  return;
-
-}
-
-void init_wp_pool() {
-  int i;
-  for (i = 0; i < NR_WP; i ++) {
-    wp_pool[i].NO = i;
-    wp_pool[i].next = (i == NR_WP - 1 ? NULL : &wp_pool[i + 1]);
-    wp_pool[i].past = (i == 0 ? NULL : &wp_pool[i - 1]);
-  }
-
-  head = NULL;
-  free_ = wp_pool;
-}
-
-/* TODO: Implement the functionality of watchpoint */
 
 
 /* ------------------------------------sdb.c------------------------------------ */
 
-static int is_batch_mode = false;
 
-/* We use the `readline' library to provide more flexibility to read from stdin. */
-static char *rl_gets()
-{
-  static char *line_read = NULL;
-
-  if (line_read)
-  {
-    free(line_read);
-    line_read = NULL;
-  }
-
-  line_read = readline("(npc) ");
-
-  if (line_read && *line_read)
-  {
-    add_history(line_read);
-  }
-
-  return line_read;
-}
-
-static char too_lessArg(char *args){
-  if(args == NULL) {
-    Log("Without necessary arguments!");
-    return 1;
-  }
-  return 0;
-}
-
-static int cmd_c(char *args)
-{
-  /*Simulate how the CPU works.*/
-  cpu_exec(-1);
-  return 0;
-}
-
-uint32_t convert_ten(char *args){
-  uint32_t flag = 1;
-  uint32_t flag_neg = 0;
-  uint32_t n = 0;
-
-  int i = strlen(args) - 1;
-  for(  ;i >= 0;i --){
-    if(args[i] == 'n'){
-      flag_neg += 1;
-      continue;
-    }
-    n += ((int)args[i] - (int)'0') * flag;
-    flag = flag * 10;
-  }
-  while(flag_neg --) n = n * (-1);
-  return n;
-}
-
-uint32_t convert_16(char *args){
-  uint32_t addr = 0;
-  uint32_t flag = 1;
-  for(int i = strlen(args) - 1;i >= 2;i --){
-    if(args[i] >= 'a' && args[i] <= 'f'){
-      addr += ((int)args[i] - (int)'a' + 10) * flag;
-    }
-    else {
-      addr += ((int)args[i] - (int)'0') * flag;
-  }
-    flag *= 16;
-  }
-  return addr;
-}
-
-static int cmd_si(char *args){
-  uint64_t n = 0;
-  if(args == NULL)  n = 1;
-  else {
-    for(int i = 0; i < strlen(args) ; i ++){
-      if(args[i] > '9' || args[i] < '0'){
-        Log("Please input number!");
-        return 0;
-      }
-    }
-    n = convert_ten(args);
-  }
-  cpu_exec(n*2);
-  return 0;
-}
-
-static int cmd_q(char *args)
-{
-  npc_state.state = NPC_QUIT;
-  return -1;
-}
-
-static void watchPoints_display(){
-  WP *index = head;
-  if(index == NULL ) {
-    printf("Now, no WP in watchPool!\n");
-    return;
-  }
-  //printf("head : %p\n",index);
-  //printf("head->next : %p\n",index->next);
-  printf("\033[92m Num \tTYpe \tDisp \tEnb \tAddress \t What \033[m \n");
-  while(index != NULL){
-    printf("\033[92m %d \thw watchpoint \tkeep \ty \t %s \033[m \n",index->NO,index->target);
-    printf("\033[96m \tbreakpoint already hit %d time \033[m \n",index->times);
-    index = index->next;
-  }
-  return;
-}
-
-static int cmd_w(char *args){
-  if(too_lessArg(args) == 1) return 0;
-  WP* ret =new_wp(args);
-  if(ret==NULL) printf("%s already in wp_pool!\n",args); 
-  watchPoints_display();
-  return 0;
-}
-
-static int cmd_pcount(char *args){
-  bool success = true;
-  if(too_lessArg(args) == 1) {
-    return 0;
-  }
-  word_t result = expr(args,&success);
-  printf("\033[105m [ %s ] = [%u] [0x%08x]\033[0m\n",args,result,result);
-  Assert(success,"Make_token fail!");
-  return 0;
-}
-
-static int cmd_x(char *args){
-  if(too_lessArg(args) == 1) return 0;
-
-  char *arg1 = strtok(NULL," ");
-  char *arg2 = strtok(NULL," ");
-  if(too_lessArg(arg2) == 1) return 0;
-
-  int len = convert_ten(arg1);
-  vaddr_t addr = convert_16(arg2);
-  printf("addr = %08x\n",addr);
-
-  for (int i = 0;i < len;i ++){
-    printf("\033[105m 0x%08x: \033[0m \t0x%08x\n",addr + i,paddr_read(addr + i,4));
-  }
-  return 0; 
-}
-
-static int cmd_info(char *args){
-  if (too_lessArg(args) == 1) return 0; 
-  else if (*args == 'r')  isa_reg_display();
-  else if (*args == 'w')  watchPoints_display();
-  else Log("Unknown command '%s'", args);
-  return 0;
-}
-
-static int cmd_help(char *args);
-
-static int cmd_d(char *args){
-  if(too_lessArg(args) == 1) return 0;
-  WP *index = head;
-  while(index != NULL){
-    if(convert_ten(args) == index->NO){
-      free_wp(index);
-      watchPoints_display();
-
-      return 0;;
-    }
-    index = index->next;
-  }
-  printf("No %s in watchpool!\n",args);
-  watchPoints_display();
-  return 0;
-}
-
-
-static struct
-{
-  const char *name;
-  const char *description;
-  int (*handler)(char *);
-} cmd_table[] = {
-    {"help", "Display information about all supported commands", cmd_help},
-    {"c", "Continue the execution of the program", cmd_c},
-    {"q", "Exit NPC", cmd_q},
-    {"si", "Execuate one by one.",cmd_si},
-    {"info","Print the state of register(r) or the content of watchpoint(w)",cmd_info},
-    {"p","Get the result of EXPR.",cmd_pcount},
-    {"x","Scan the memory.",cmd_x},
-    {"w","Set a watchpoint.",cmd_w},
-    {"d","Delete certain watchpoint.",cmd_d},
-
-    /* TODO: Add more commands */
-
-};
-
-#define NR_CMD ARRLEN(cmd_table)
-
-static int cmd_help(char *args)
-{
-  /* extract the first argument */
-  char *arg = strtok(NULL, " ");
-  int i;
-
-  if (arg == NULL)
-  {
-    /* no argument given */
-    for (i = 0; i < NR_CMD; i++)
-    {
-      printf("%s - %s\n", cmd_table[i].name, cmd_table[i].description);
-    }
-  }
-  else
-  {
-    for (i = 0; i < NR_CMD; i++)
-    {
-      if (strcmp(arg, cmd_table[i].name) == 0)
-      {
-        printf("%s - %s\n", cmd_table[i].name, cmd_table[i].description);
-        return 0;
-      }
-    }
-    printf("Unknown command '%s'\n", arg);
-  }
-  return 0;
-}
-
-void sdb_set_batch_mode()
-{
-  is_batch_mode = true;
-}
-#define AUTO_Q 1
-/* Receive commands from user. */
-void sdb_mainloop()
-{
-  if (is_batch_mode)
-  {
-    cmd_c(NULL);
-    //return;
-  }
-  #ifdef AUTO_Q
-  char arr[2] = {'a','b'};
-  cmd_q(arr);
-  return;
-
-  #endif
-  for (char *str; (str = rl_gets()) != NULL;)
-  {
-    char *str_end = str + strlen(str);
-
-    /* extract the first token as the command */
-    char *cmd = strtok(str, " ");
-    if (cmd == NULL)
-    {
-      continue;
-    }
-
-    /* treat the remaining string as the arguments,
-     * which may need further parsing
-     */
-    char *args = cmd + strlen(cmd) + 1;
-    if (args >= str_end)
-    {
-      args = NULL;
-    }
-
-#ifdef CONFIG_DEVICE
-    extern void sdl_clear_event_queue();
-    sdl_clear_event_queue();
-#endif
-
-    int i;
-    for (i = 0; i < NR_CMD; i++)
-    {
-      if (strcmp(cmd, cmd_table[i].name) == 0)
-      {
-        if (cmd_table[i].handler(args) < 0)
-        {
-          return;
-        }
-        break;
-      }
-    }
-
-    if (i == NR_CMD)
-    {
-      printf("Unknown command '%s'\n", cmd);
-    }
-  }
-  WP *head = head;
-  while(head != NULL){
-    free(head->target);
-    head->target = NULL;
-    head = head->next;
-  }
-  // iringbuf *head_i = get_head_iringbuf();
-  // while(head_i != NULL && head_i->rbuf != NULL){
-  //   free(head_i->rbuf);
-  //   head_i->rbuf = NULL;
-  //   head_i = head_i->next;
-  // }
-  
-}
 
 /* start CPU or receive commands */
 void engine_start() {
