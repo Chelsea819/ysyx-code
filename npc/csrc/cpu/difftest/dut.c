c322e922d3bc9dac86672779a365ae6c2a1cd2cb
tracer-ysyx <tracer@ysyx.org>
2024-08-03 20:25:03 +0800

    > sim RTL ysyx_22041211 王晓雪 Linux chelsea-virtual-machine 5.19.0-35-generic #36~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Feb 17 15:17:25 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:25:03 up 10:41,  1 user,  load average: 1.64, 0.93, 1.09

--- npc/csrc/cpu/difftest/dut.c
+++ npc/csrc/cpu/difftest/dut.c
@@ -117,7 +117,7 @@ void init_difftest(char *ref_so_file, long img_size, int port) {
   Log("The result of every instruction will be compared with %s. "
       "This will help you a lot for debugging, but also significantly reduce the performance. "
       "If it is not necessary, you can turn it off in menuconfig.", ref_so_file);
-  Log("cpu addr: %p",&cpu);
+  Log("cpu.pc addr: %p",&(cpu.pc));
   //对REF的DIffTest功能进行初始化
   ref_difftest_init(port);
   
