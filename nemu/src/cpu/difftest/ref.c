2dd5f373538e96d2c5f60b760c48c41c98290cac
tracer-ysyx <tracer@ysyx.org>
2024-08-03 21:05:46 +0800

    > compile NEMU ysyx_22041211 王晓雪 Linux chelsea-virtual-machine 5.19.0-35-generic #36~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Feb 17 15:17:25 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:05:46 up 11:21,  1 user,  load average: 2.15, 2.37, 1.51

--- nemu/src/cpu/difftest/ref.c
+++ nemu/src/cpu/difftest/ref.c
@@ -33,7 +33,6 @@ __EXPORT void difftest_memcpy(paddr_t addr, void *buf, size_t n, bool direction)
 // `direction`为`DIFFTEST_TO_REF`时, 设置REF的寄存器状态为`dut`;
 __EXPORT void difftest_regcpy(void *dut, bool direction) {
   CPU_state *ref = &cpu;
-  printf("dut addr: %p\nref-cpu addr: %p\n",dut,ref);
   if (direction == DIFFTEST_TO_REF) {
     memcpy(ref, dut, DIFFTEST_REG_SIZE);
     if(!initial) {ref->pc = 0x80000000; initial = true;}
--- npc/csrc/cpu/cpu-exe.c
+++ npc/csrc/cpu/cpu-exe.c
@@ -528,7 +528,6 @@ static void execute(uint64_t n)
     //由于rtl对reg的更改是在下一个时钟周期上升沿，而nemu对reg的更改是即时的
     //所以这里要整个往后延迟一个周期
     if(cpu.pc != 0x80000000 && dut.clk == 1) {
-      printf("cpu.pc = 0x%08x dut.pc = 0x%08x s.npc = 0x%08x\n",cpu.pc,dut.pc,s.dnpc);
       // printf("inst = 0x%08x\n",dut.rootp->ysyx_22041211_top__DOT__inst);
       trace_and_difftest(diff.dnpc);
     }
