faabea661f269f56aca31f683cba16837d62ddcb
tracer-ysyx <tracer@ysyx.org>
2024-08-04 11:23:22 +0800

    > sim RTL ysyx_22041211 王晓雪 Linux chelsea-virtual-machine 5.19.0-35-generic #36~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Feb 17 15:17:25 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  11:23:22 up 15:20,  1 user,  load average: 1.32, 1.11, 0.95

--- npc/csrc/device/vga.c
+++ npc/csrc/device/vga.c
@@ -73,7 +73,7 @@ static inline void update_screen() {
 void vga_update_screen() {
   
   if(vgactl_port_base[1]) {
-    // printf("enter vgactl_port_base[1] %d\n",vgactl_port_base[1]);
+    printf("enter vgactl_port_base[1] %d\n",vgactl_port_base[1]);
     update_screen();printf("vga_update_screen\n");
     vgactl_port_base[1] = 0;
     // io_write(CONFIG_VGA_CTL_MMIO + 4, 0);
