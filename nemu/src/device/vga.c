d85210297a36631c2977029009f39c6f43582f63
tracer-ysyx <tracer@ysyx.org>
2024-08-04 11:26:10 +0800

    > compile NEMU ysyx_22041211 王晓雪 Linux chelsea-virtual-machine 5.19.0-35-generic #36~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Feb 17 15:17:25 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  11:26:10 up 15:22,  1 user,  load average: 1.65, 1.34, 1.07

--- nemu/src/device/vga.c
+++ nemu/src/device/vga.c
@@ -73,6 +73,7 @@ static inline void update_screen() {
 
 void vga_update_screen() {
   // printf("vgactl_port_base[1] %d\n",vgactl_port_base[1]);
+   printf("1");
   if(vgactl_port_base[1]) {
     printf("enter vgactl_port_base[1] %d\n",vgactl_port_base[1]);
     update_screen();
