f0530c7212b908a09aba887eaaecb4c1746e1df0
tracer-ysyx <tracer@ysyx.org>
2024-08-09 00:19:48 +0800

    > sim RTL ysyx_22041211 王晓雪 Linux chelsea-virtual-machine 5.19.0-35-generic #36~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Feb 17 15:17:25 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  00:19:48 up  2:29,  0 users,  load average: 0.29, 0.23, 0.39

--- npc/vsrc/ysyx_22041211_top.v
+++ npc/vsrc/ysyx_22041211_top.v
@@ -85,7 +85,7 @@ module ysyx_22041211_top #(parameter DATA_LEN = 32,ADDR_LEN = 32) (
 	always @(posedge clk) begin
 		if(~rst) begin
         	if_inst <= pmem_read_task(if_pc_next, 8'b00001111);
-			$display("pc: [%h] inst: [%h]",if_pc_next, if_inst);
+			$display("pc: [%h] inst: [%h] invalid: [%h]",if_pc_next, if_inst, invalid);
 		end
 	end
 
