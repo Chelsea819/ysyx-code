2b66a27198199134d1bb6ea96276b82da0d16317
tracer-ysyx <tracer@ysyx.org>
2024-08-09 10:48:11 +0800

    > sim RTL ysyx_22041211 王晓雪 Linux chelsea-virtual-machine 5.19.0-35-generic #36~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Feb 17 15:17:25 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  10:48:11 up  4:21,  1 user,  load average: 2.30, 1.02, 0.73

--- npc/vsrc/ysyx_22041211_counter.v
+++ npc/vsrc/ysyx_22041211_counter.v
@@ -5,7 +5,7 @@
 	> Created Time: 2023年08月05日 星期六 22时12分23秒
  ************************************************************************/
 
-module ysyx_22041211_counter #(parameter ADDR_LEN = 32,RESET_VAL = 32'h80000000)(
+module ysyx_22041211_counter #(parameter ADDR_LEN = 32)(
 	input									clk		,
 	input									rst		,
 	input	    [ADDR_LEN - 1:0]			pc_next	,
@@ -14,10 +14,7 @@ module ysyx_22041211_counter #(parameter ADDR_LEN = 32,RESET_VAL = 32'h80000000)
 );
 	// ysyx_22041211_Reg #(ADDR_LEN, RESET_VAL) PC_Reg (clk,rst,pc_next,1'b1,pc);
 	always @ (posedge clk) begin
-		if(rst)
-		 	pc <= RESET_VAL;
-		else 
-			pc <= pc_next;
+		pc <= pc_next;
 	end
 
 endmodule
