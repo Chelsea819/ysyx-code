88d8c2442f3906ccea7ccf20af37f68100b8c46a
tracer-ysyx <tracer@ysyx.org>
2024-08-09 09:51:27 +0800

    > sim RTL ysyx_22041211 王晓雪 Linux chelsea-virtual-machine 5.19.0-35-generic #36~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Feb 17 15:17:25 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  09:51:27 up  3:24,  1 user,  load average: 1.12, 0.54, 0.37

--- npc/vsrc/ysyx_22041211_top.v
+++ npc/vsrc/ysyx_22041211_top.v
@@ -84,13 +84,13 @@ module ysyx_22041211_top #(parameter DATA_LEN = 32,ADDR_LEN = 32) (
 	//取指令
 	always @(posedge clk) begin
 		if(~rst) begin
-        	if_inst <= pmem_read_task(if_pc_next, 8'b00001111);
-			$display("pc: [%h] inst: [%h] invalid: [%h]",if_pc_next, if_inst, invalid);
+        	if_inst <= pmem_read_task(id_pc_i, 8'b00001111);
+			$display("pc: [%h] inst: [%h] invalid: [%h]",id_pc_i, if_inst, invalid);
 		end
 	end
 
 	always @(*) begin
-			$display("pc: [%h] inst: [%h] invalid: [%h]",if_pc_next, if_inst, invalid);
+			$display("pc: [%h] inst: [%h] invalid: [%h]",id_pc_i, if_inst, invalid);
 	end
 
 	assign id_inst_i = if_inst;
