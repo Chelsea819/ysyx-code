45e920a5acdd3f5e70850bcf8c4d50a37714e072
tracer-ysyx <tracer@ysyx.org>
2024-08-10 16:21:05 +0800

    > sim RTL ysyx_22041211 王晓雪 Linux chelsea-virtual-machine 5.19.0-35-generic #36~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Feb 17 15:17:25 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:21:05 up 19:52,  1 user,  load average: 0.35, 0.79, 0.63

--- npc/csrc/isa/difftest/riscv32-dut.c
+++ npc/csrc/isa/difftest/riscv32-dut.c
@@ -11,7 +11,7 @@ extern TOP_NAME *dut; extern VerilatedVcdC *m_trace;
 bool isa_difftest_checkregs(CPU_state *ref_r, vaddr_t pc) {
   if(cpu.pc!= ref_r->pc) return false;
   for(int i = 0; i < 32; i++){
-    if(ref_r->gpr[i] != R(i)) return false;
+    if(ref_r->gpr[i] != cpu.gpr[i]) return false;
   } 
   return true;
 }
