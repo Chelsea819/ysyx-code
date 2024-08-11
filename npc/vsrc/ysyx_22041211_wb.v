afcab703b2c902bda42939f927bdf26ac331c583
tracer-ysyx <tracer@ysyx.org>
2024-08-11 12:07:46 +0800

    > sim RTL ysyx_22041211 王晓雪 Linux chelsea-virtual-machine 5.19.0-35-generic #36~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Feb 17 15:17:25 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  12:07:46 up 1 day, 15:39,  1 user,  load average: 4.86, 1.68, 1.21

--- npc/vsrc/ysyx_22041211_wb.v
+++ npc/vsrc/ysyx_22041211_wb.v
@@ -35,17 +35,17 @@ module ysyx_22041211_wb #(parameter DATA_LEN = 32)(
    
     // 访存指令
     import "DPI-C" context function int pmem_read_task(input int raddr, input byte wmask);
-     assign mem_rdata_rare = mem_raddr != 0 ? pmem_read_task(mem_raddr, mem_rmask) : 0;
-	// always @(*) begin
+     //assign mem_rdata_rare = mem_raddr != 0 ? pmem_read_task(mem_raddr, mem_rmask) : 0;
+	always @(*) begin
         
-    //     if(rst) 
-    //         mem_rdata_rare = 0;
-    //     else if(mem_to_reg ) begin
-    //         $display("rst = %b",rst);mem_rdata_rare = 0;
-    //         //mem_rdata_rare = mem_raddr != 0 ? pmem_read_task(mem_raddr, mem_rmask) : 0;
-    //     end else 
-    //         mem_rdata_rare = 0;
-	// end
+        if(rst) 
+            mem_rdata_rare = 0;
+        else if(mem_to_reg ) begin
+            $display("rst = %b",rst);mem_rdata_rare = 0;
+            mem_rdata_rare = pmem_read_task(mem_raddr, mem_rmask);
+        end else 
+            mem_rdata_rare = 0;
+	end
 	import "DPI-C" function void pmem_write_task(input int waddr, input int wdata);
 	always @(*) begin
   		if (mem_wen_i) // 有写请求时
