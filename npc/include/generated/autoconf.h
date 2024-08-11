415253064d020c27d47ab0243dc152c234038a0d
tracer-ysyx <tracer@ysyx.org>
2024-08-11 23:17:52 +0800

    > sim RTL ysyx_22041211 王晓雪 Linux chelsea-virtual-machine 5.19.0-35-generic #36~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Feb 17 15:17:25 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  23:17:52 up 1 day, 19:53,  1 user,  load average: 0.75, 0.94, 0.64

--- npc/include/generated/autoconf.h
+++ npc/include/generated/autoconf.h
@@ -14,6 +14,7 @@
 #define CONFIG_MSIZE 0x8000000
 #define CONFIG_CC_O2 1
 #define CONFIG_MODE_SYSTEM 1
+#define CONFIG_ITRACE 1
 #define CONFIG_DIFFTEST 1
 #define CONFIG_ISA_riscv 1
 #define CONFIG_TRACE_END 10000
@@ -25,6 +26,7 @@
 #define CONFIG_CC_OPT "-O2"
 #define CONFIG_MTRACE 1
 #define CONFIG_RT_CHECK 1
+#define CONFIG_ITRACE_COND "true"
 #define CONFIG_CC "gcc"
 #define CONFIG_DIFFTEST_REF_PATH "/home/chelsea/ysyx-workbench/nemu"
 #define CONFIG_CC_DEBUG 1
