fd5e706f27dcdee817c39710d92b96a48b077a17
tracer-ysyx <tracer@ysyx.org>
2024-08-11 12:33:55 +0800

    > sim RTL ysyx_22041211 王晓雪 Linux chelsea-virtual-machine 5.19.0-35-generic #36~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Feb 17 15:17:25 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  12:33:55 up 1 day, 16:05,  1 user,  load average: 1.22, 0.85, 0.88

--- npc/vsrc/ysyx_22041211_wb.v
+++ npc/vsrc/ysyx_22041211_wb.v
@@ -42,6 +42,7 @@ module ysyx_22041211_wb #(parameter DATA_LEN = 32)(
             $display("mem_to_reg = %b",mem_to_reg);
         end
         else if(mem_to_reg) begin
+             $display("mem_to_reg = %b",mem_to_reg);
             $display("rst = %b",rst); mem_rdata_rare = 0;
             //mem_rdata_rare = pmem_read_task(mem_raddr, mem_rmask);
         end else 
