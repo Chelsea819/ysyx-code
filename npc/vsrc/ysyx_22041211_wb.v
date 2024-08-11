a1117815a6a6c1a8a04c4529108e8273a19ab72a
tracer-ysyx <tracer@ysyx.org>
2024-08-11 12:55:08 +0800

    > sim RTL ysyx_22041211 王晓雪 Linux chelsea-virtual-machine 5.19.0-35-generic #36~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Feb 17 15:17:25 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  12:55:08 up 1 day, 16:26,  1 user,  load average: 1.00, 0.71, 0.70

--- npc/vsrc/ysyx_22041211_wb.v
+++ npc/vsrc/ysyx_22041211_wb.v
@@ -42,13 +42,13 @@ module ysyx_22041211_wb #(parameter DATA_LEN = 32)(
             $display("mem_to_reg = %b",mem_to_reg);
         end
         else begin
-            // if(mem_to_reg)begin 
-            // $display("mem_to_reg = %b",mem_to_reg); // mem_rdata_rare = 0;
-            // mem_rdata_rare = pmem_read_task(mem_raddr, mem_rmask);
-            // end
-            //else begin
+            if(mem_to_reg)begin 
+            //  $display("mem_to_reg = %b",mem_to_reg); // mem_rdata_rare = 0;
+            mem_rdata_rare = pmem_read_task(mem_raddr, mem_rmask);
+            end
+            else begin
                 mem_rdata_rare = 0;
-            //end
+            end
         end
 	end
 	import "DPI-C" function void pmem_write_task(input int waddr, input int wdata);
