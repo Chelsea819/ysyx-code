9fc540369fe42098dd627bcbf2c8ab9b46879104
tracer-ysyx <tracer@ysyx.org>
2024-08-12 20:55:14 +0800

    > sim RTL ysyx_22041211 王晓雪 Linux chelsea-virtual-machine 5.19.0-35-generic #36~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Feb 17 15:17:25 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:55:14 up 2 days,  6:51,  1 user,  load average: 4.24, 1.31, 0.68

--- npc/vsrc/ysyx_22041211_top.v
+++ npc/vsrc/ysyx_22041211_top.v
@@ -80,6 +80,10 @@ module ysyx_22041211_top #(parameter DATA_LEN = 32,ADDR_LEN = 32) (
     always @(*)
         ifebreak_func(id_inst_i);
 
+	// 为ITRACE提供指令
+    import "DPI-C" context function void inst_get(int inst);
+    always @(*)
+        inst_get(id_inst_i);
 
 	import "DPI-C" context function int pmem_read_task(input int raddr, input byte wmask);
 	import "DPI-C" function void pmem_write_task(input int waddr, input int wdata);
