e16fd8eeb0818fa241cfd0ee1eb25af6755cfda2
tracer-ysyx <tracer@ysyx.org>
2024-08-12 14:55:22 +0800

    > sim RTL ysyx_22041211 王晓雪 Linux chelsea-virtual-machine 5.19.0-35-generic #36~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Feb 17 15:17:25 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  14:55:22 up 2 days, 52 min,  1 user,  load average: 0.64, 0.44, 0.35

--- npc/vsrc/ysyx_22041211_ALU.v
+++ npc/vsrc/ysyx_22041211_ALU.v
@@ -24,8 +24,8 @@ module ysyx_22041211_ALU #(parameter DATA_LEN = 32)(
 	wire 								sub_cout;
 
 	always @(*) begin
-		$display("src1 = [%b], src2 = [%b] alu_zero_o = [%b]",src1, src2, alu_zero_o);
-		$display("src1 = [%h], src2 = [%h]",src1, src2);
+		$display("src1 = [%b], src2 = [%b] alu_zero_o = [%b]  alu_less = [%b]",src1, src2, alu_zero_o, alu_less_o);
+		$display("src1 = [%h], src2 = [%h] sub_result = [%b] s_compare_result = [%b]",src1, src2, sub_result, s_compare_result);
 	end
 	
 	ysyx_22041211_MuxKeyWithDefault #(10,4,32) ALUmode (result_tmp, alu_control, 32'b0, {
