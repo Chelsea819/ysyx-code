99de889a326d1a5b6ab3f0674743cf788c0098aa
tracer-ysyx <tracer@ysyx.org>
2024-08-15 17:14:48 +0800

    > sim RTL ysyx_22041211 王晓雪 Linux chelsea-virtual-machine 5.19.0-35-generic #36~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Feb 17 15:17:25 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:14:48 up 3 days,  6:09,  1 user,  load average: 1.13, 0.68, 0.47

--- npc/csrc/isa/difftest/riscv32-dut.c
+++ npc/csrc/isa/difftest/riscv32-dut.c
@@ -1,3 +1,4 @@
+#include <cstdio>
 #include <isa.h>
 #include <cpu/difftest.h>
 #include "reg.h"
@@ -13,6 +14,7 @@ int isa_difftest_checkregs(CPU_state *ref_r, vaddr_t pc) {
   for(int i = 0; i < RISCV_GPR_NUM; i++){
     if(ref_r->gpr[i] != cpu.gpr[i]) return (i + 1);
   } 
+  printf("ref_r->mcause = 0x%08x cpu.mcause = 0x%08x\n",ref_r->mcause, cpu.mcause);
   if (ref_r->mcause != cpu.mcause) {
     return 34;
   } else if (0) {
