ecccc40c87cb5f6cce55643d106c1f81b3a8e8e5
tracer-ysyx <tracer@ysyx.org>
2024-08-15 17:21:23 +0800

    > sim RTL ysyx_22041211 王晓雪 Linux chelsea-virtual-machine 5.19.0-35-generic #36~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Feb 17 15:17:25 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:21:23 up 3 days,  6:16,  1 user,  load average: 0.59, 0.65, 0.52

--- abstract-machine/am/src/riscv/npc/cte.c
+++ abstract-machine/am/src/riscv/npc/cte.c
@@ -7,7 +7,7 @@ static Context* (*user_handler)(Event, Context*) = NULL;
 Context* __am_irq_handle(Context *c) {
   if (user_handler) {
     Event ev = {0};
-    // asm volatile ("ebreak");
+    asm volatile ("ebreak");
     switch (c->mcause) {
       case 0xb:
         if (c->GPR1 == -1) ev.event = EVENT_YIELD;    // 系统调用号的识别
