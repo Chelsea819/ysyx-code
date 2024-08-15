1f35cf98110bf2c85ea88370a7a2c7bd99b4307a
tracer-ysyx <tracer@ysyx.org>
2024-08-15 16:52:28 +0800

    > compile NEMU ysyx_22041211 王晓雪 Linux chelsea-virtual-machine 5.19.0-35-generic #36~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Feb 17 15:17:25 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:52:28 up 3 days,  5:47,  1 user,  load average: 0.45, 0.36, 0.31

--- nemu/src/memory/paddr.c
+++ nemu/src/memory/paddr.c
@@ -65,7 +65,7 @@ void init_mem() {
   uint32_t *p = (uint32_t *)pmem;
   int i;
   for (i = 0; i < (int) (CONFIG_MSIZE / sizeof(p[0])); i ++) {
-    p[i] = rand();
+    p[i] = 0;
   }
 #endif
   /*打印日志,输出初始化的内存区域的地址范围,
