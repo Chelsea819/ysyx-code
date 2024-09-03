851d4adb5e640e6130888d83e6958accc8dcc9fb
tracer-ysyx <tracer@ysyx.org>
2024-09-03 20:18:56 +0800

    > sim RTL ysyx_22041211 王晓雪 Linux chelsea-virtual-machine 5.19.0-35-generic #36~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Feb 17 15:17:25 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:18:56 up 2 days,  6:06,  1 user,  load average: 1.45, 1.06, 0.98

--- npc/include/generated/autoconf.h
+++ npc/include/generated/autoconf.h
@@ -4,17 +4,16 @@
  * NPC Configuration Menu
  *
  */
-#define CONFIG_DIFFTEST_REF_NAME "nemu"
+#define CONFIG_DIFFTEST_REF_NAME "none"
 #define CONFIG_ENGINE "interpreter"
 #define CONFIG_PC_RESET_OFFSET 0x0
 #define CONFIG_RTC_MMIO 0xa0000048
 #define CONFIG_TARGET_NATIVE_ELF 1
-#define CONFIG_DIFFTEST_REF_NEMU 1
 #define CONFIG_MSIZE 0x8000000
 #define CONFIG_CC_O2 1
 #define CONFIG_DEVICE 1
 #define CONFIG_MODE_SYSTEM 1
-#define CONFIG_DIFFTEST 1
+#define CONFIG_MEM_RANDOM 1
 #define CONFIG_HAS_SERIAL 1
 #define CONFIG_ISA_riscv 1
 #define CONFIG_CC_LTO 1
@@ -26,7 +25,7 @@
 #define CONFIG_SERIAL_MMIO 0xa00003f8
 #define CONFIG_RT_CHECK 1
 #define CONFIG_CC "gcc"
-#define CONFIG_DIFFTEST_REF_PATH "/home/chelsea/ysyx-workbench/nemu"
+#define CONFIG_DIFFTEST_REF_PATH "none"
 #define CONFIG_RVE 1
 #define CONFIG_CC_GCC 1
 #define CONFIG_ISA "riscv32"
