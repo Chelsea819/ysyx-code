3c27931c4cf27ed46f7f355c8b5e537ad3e093f4
tracer-ysyx <tracer@ysyx.org>
2024-09-06 16:12:22 +0800

    > sim RTL ysyx_22041211 王晓雪 Linux chelsea-virtual-machine 5.19.0-35-generic #36~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Feb 17 15:17:25 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:12:22 up 3 days,  2:48,  1 user,  load average: 4.11, 1.98, 1.20

--- npc/vsrc/ysyx_22041211_data_SRAM.v
+++ npc/vsrc/ysyx_22041211_data_SRAM.v
@@ -18,7 +18,6 @@ module ysyx_22041211_data_SRAM #(parameter ADDR_LEN = 32, DATA_LEN = 32)(
 	input		[7:0]  					mem_rmask	,
     output	reg	[DATA_LEN - 1:0]		mem_rdata_usigned_o
 );	
-	wire [31:0] mem_waddr;
 
 	// 访存指令
     import "DPI-C" context function int pmem_read_task(input int raddr, input byte wmask);
@@ -33,8 +32,8 @@ module ysyx_22041211_data_SRAM #(parameter ADDR_LEN = 32, DATA_LEN = 32)(
 	import "DPI-C" function void pmem_write_task(input int waddr, input int wdata, input byte wmask);
 	always @(posedge clk) begin
   		if (mem_wen_i) begin // 有写请求时
-			$display("mem_wen_i = %d mem_waddr = %x\n",mem_wen_i, mem_waddr_i);
-            pmem_write_task(mem_waddr, mem_wdata_i, mem_wmask);
+			// $display("mem_wen_i = %d mem_waddr = %x\n",mem_wen_i, mem_waddr_i);
+            pmem_write_task(mem_waddr_i, mem_wdata_i, mem_wmask);
 		end
 	end
 
