b23d00823ce2e7ca468658b1975b36c4890c2c95
tracer-ysyx <tracer@ysyx.org>
2024-09-06 15:51:26 +0800

    > sim RTL ysyx_22041211 王晓雪 Linux chelsea-virtual-machine 5.19.0-35-generic #36~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Feb 17 15:17:25 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:51:26 up 3 days,  2:27,  1 user,  load average: 0.36, 0.42, 0.59

--- npc/csrc/cpu/cpu-exe.c
+++ npc/csrc/cpu/cpu-exe.c
@@ -549,6 +549,7 @@ static void execute(uint64_t n) {
     //由于rtl对reg的更改是在下一个时钟周期上升沿，而nemu对reg的更改是即时的
     //所以这里要整个往后延迟一个周期
     // if(cpu.pc != 0x80000000) {
+    printf("trace and diff\n");
       trace_and_difftest();
 // }
   #ifdef CONFIG_DIFFTEST
