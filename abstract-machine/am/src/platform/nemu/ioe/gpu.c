d68451fd5e2d4e55efc1822f54b1ac79bb9ddc3d
tracer-ysyx <tracer@ysyx.org>
2024-09-14 17:26:10 +0800

    > compile NEMU ysyx_22041211 王晓雪 Linux chelsea-virtual-machine 5.19.0-35-generic #36~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Feb 17 15:17:25 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:26:10 up 6 days,  6:24,  6 users,  load average: 0.45, 0.28, 0.21

--- abstract-machine/am/src/platform/nemu/ioe/gpu.c
+++ abstract-machine/am/src/platform/nemu/ioe/gpu.c
@@ -1,19 +1,19 @@
 #include <am.h>
 #include <nemu.h>
-#define MODE_800x600
-#ifdef MODE_800x600
-# define W    800
-# define H    600
-#else
-# define W    400
-# define H    300
-#endif
+// #define MODE_800x600
+// #ifdef MODE_800x600
+// # define W    800
+// # define H    600
+// #else
+// # define W    400
+// # define H    300
+// #endif
 
-#define SCREEN_W (MUXDEF(CONFIG_VGA_SIZE_800x600, 800, 400))
-#define SCREEN_H (MUXDEF(CONFIG_VGA_SIZE_800x600, 600, 300))
+// #define SCREEN_W (MUXDEF(CONFIG_VGA_SIZE_800x600, 800, 400))
+// #define SCREEN_H (MUXDEF(CONFIG_VGA_SIZE_800x600, 600, 300))
 
 #define SYNC_ADDR (VGACTL_ADDR + 4)
-
+static int WIDTH = 0;
 void __am_gpu_init() {
   // int i;
   // int w = W;// TODO: get the correct width
@@ -24,27 +24,29 @@ void __am_gpu_init() {
 }
 
 void __am_gpu_config(AM_GPU_CONFIG_T *cfg) {
+  uint32_t size = inl(VGACTL_ADDR + 0);
+  WIDTH = (int)(size >> 16);
   *cfg = (AM_GPU_CONFIG_T) {
     .present = true, .has_accel = false,
-    .width = W, .height = H,
+    .width = WIDTH, .height = (size & 0xffff),
     .vmemsz = 0
   };
 }
 
 void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl) { 
-  // if(ctl->sync){
     int x = ctl->x, y = ctl->y, w = ctl->w, h = ctl->h;
+    if (WIDTH == 0) {
+      WIDTH = (int)(inl(VGACTL_ADDR + 0) >> 16);
+    }
     uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
     uint32_t *p = (uint32_t *)ctl->pixels;
     int k = 0;
     for(int i = y; i < y + h; i ++){
       for(int j = x; j < x + w; j ++){
-        fb[W*i+j] = p[k++];
+        fb[WIDTH * i + j] = p[k++];
       }
     }
     outl(SYNC_ADDR, 1);
-  // }
-  
 }
 
 void __am_gpu_status(AM_GPU_STATUS_T *status) {
