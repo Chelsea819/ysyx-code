7c5105c92d176b887e195c3d285eb123c435cb72
tracer-ysyx <tracer@ysyx.org>
2024-09-19 21:59:30 +0800

    > sim RTL ysyx_22041211 王晓雪 Linux chelsea-virtual-machine 5.19.0-35-generic #36~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Feb 17 15:17:25 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:59:30 up 7 days, 20:16,  7 users,  load average: 1.55, 0.48, 0.34

--- npc/csrc/memory/paddr.c
+++ npc/csrc/memory/paddr.c
@@ -120,9 +120,9 @@ vaddr_t paddr_read(paddr_t addr,int len) {
   
 	if (likely(in_pmem(addr))) {
     word_t rdata = pmem_read(addr,len);
-    // #ifdef CONFIG_MTRACE
-    //   Log("paddr_read ---  [addr: 0x%08x len: %d rdata: 0x%08x]",addr,len,rdata);
-    // #endif
+    #ifdef CONFIG_MTRACE
+      Log("paddr_read ---  [addr: 0x%08x len: %d rdata: 0x%08x]",addr,len,rdata);
+    #endif
     return rdata;
   }
   // printf("read device\n");
