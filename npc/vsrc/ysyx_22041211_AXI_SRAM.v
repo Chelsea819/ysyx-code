79fd196daf561b15ba9fa4fc0ba02a2bb314e53d
tracer-ysyx <tracer@ysyx.org>
2024-09-21 20:07:41 +0800

    > sim RTL ysyx_22041211 王晓雪 Linux chelsea-virtual-machine 5.19.0-35-generic #36~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Feb 17 15:17:25 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:07:41 up 8 days, 14:45,  8 users,  load average: 1.77, 1.89, 2.04

--- npc/vsrc/ysyx_22041211_AXI_SRAM.v
+++ npc/vsrc/ysyx_22041211_AXI_SRAM.v
@@ -75,16 +75,20 @@ module ysyx_22041211_AXI_SRAM #(parameter ADDR_LEN = 32, DATA_LEN = 32)(
 
   // r addr delay
 	always @(posedge clk ) begin
-		if(next_state == WAIT_DATA_GET && r_valid_delay != RANDOM_DELAY)
+		if(next_state == WAIT_DATA_GET && r_valid_delay == RANDOM_DELAY)
+			r_valid_delay <= r_valid_delay;
+		else if (next_state == WAIT_DATA_GET && r_valid_delay != RANDOM_DELAY) begin
 			r_valid_delay <= r_valid_delay + 1;
-		else 
+		end
 			r_valid_delay <= 4'b0;
 	end
 
 	always @(posedge clk ) begin
-		if(next_state == WAIT_DATA_WRITE && bkwd_valid_delay != RANDOM_DELAY)
+		if(next_state == WAIT_DATA_WRITE && bkwd_valid_delay == RANDOM_DELAY)
+			bkwd_valid_delay <= bkwd_valid_delay;
+		else if (next_state == WAIT_DATA_WRITE && bkwd_valid_delay != RANDOM_DELAY) begin
 			bkwd_valid_delay <= bkwd_valid_delay + 1;
-		else 
+		end else
 			bkwd_valid_delay <= 4'b0;
 	end
 // no delay
