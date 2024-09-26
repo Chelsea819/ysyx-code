aa4e81b3cb84408a9a10bbee8e726984e29a7217
tracer-ysyx <tracer@ysyx.org>
2024-09-26 13:20:42 +0800

    > sim RTL ysyx_22041211 王晓雪 Linux chelsea-virtual-machine 5.19.0-35-generic #36~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Feb 17 15:17:25 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  13:20:42 up 10 days,  3:52,  8 users,  load average: 3.48, 1.87, 1.59

--- npc/vsrc/ysyx_22041211_define_delay.v
+++ npc/vsrc/ysyx_22041211_define_delay.v
@@ -1,10 +1,10 @@
 // delay test
-// `define DELAY_TEST 1'b1
+`define DELAY_TEST 1'b1
     `ifdef DELAY_TEST
-        `define RAN_DELAY 1'b1
-        // `define VAR_DELAY 4'b101
+        // `define RAN_DELAY 1'b1
+        `define VAR_DELAY 4'b010
         `ifdef VAR_DELAY
-            `define VAR_W_DELAY 4'b111
+            `define VAR_W_DELAY 4'b011
         `endif
     `endif
 
