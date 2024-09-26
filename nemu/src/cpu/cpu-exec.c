433333466e9bfbd93b79114ee3aae9bfdae6ef5a
tracer-ysyx <tracer@ysyx.org>
2024-09-26 18:45:18 +0800

    > compile NEMU ysyx_22041211 王晓雪 Linux chelsea-virtual-machine 5.19.0-35-generic #36~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Feb 17 15:17:25 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  18:45:18 up 10 days,  8:14,  8 users,  load average: 2.13, 1.82, 2.12

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -495,7 +495,7 @@ void cpu_exec(uint64_t n)
   g_print_step = (n < MAX_INST_TO_PRINT);
   switch (nemu_state.state)
   {
-  case NEMU_END:
+  // case NEMU_END:
   case NEMU_ABORT:
     printf("Program execution has ended. To restart the program, exit NEMU and run again.\n");
     return;
