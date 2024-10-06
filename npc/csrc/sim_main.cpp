b3b7df46c4b2522aad9c936bfb1ec562aa56e46a
tracer-ysyx <tracer@ysyx.org>
2024-10-06 20:30:04 +0800

    > sim RTL ysyx_22041211 王晓雪 Linux chelsea-virtual-machine 5.19.0-35-generic #36~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Feb 17 15:17:25 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:30:04 up 10:51,  2 users,  load average: 0.38, 0.34, 0.29

--- npc/csrc/sim_main.cpp
+++ npc/csrc/sim_main.cpp
@@ -30,9 +30,17 @@ extern WP *head;
 VerilatedVcdC *m_trace = new VerilatedVcdC;
 #endif
 
+static const uint32_t img[] = {
+    0x00000297, // auipc t0,0
+    0x00028823, // sb  zero,16(t0)
+    0x0102c503, // lbu a0,16(t0)
+    0x00100073, // ebreak (used as nemu_trap)
+    0xdeadbeef, // some data
+};
+
 extern "C" void flash_read(int32_t addr, int32_t *data) { assert(0); }
 extern "C" void mrom_read(int32_t addr, int32_t *data) {
-  *data = 0b00000000000100000000000001110011;
+  *data = img[addr - 0x20000000];
 }
 
 int main(int argc, char **argv, char **env) {
