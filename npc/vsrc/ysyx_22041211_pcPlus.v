14d81d9feedb43df03dd9b44c89b754f6e0b831e
tracer-ysyx <tracer@ysyx.org>
2024-10-08 22:28:31 +0800

    > sim RTL ysyx_22041211 王晓雪 Linux chelsea-virtual-machine 5.19.0-35-generic #36~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Feb 17 15:17:25 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  22:28:31 up 1 day,  5:26,  2 users,  load average: 0.55, 0.33, 0.36

--- npc/vsrc/ysyx_22041211_pcPlus.v
+++ npc/vsrc/ysyx_22041211_pcPlus.v
@@ -4,7 +4,7 @@
 	> Mail: 1938166340@qq.com 
 	> Created Time: 2023年08月04日 星期五 18时24分15秒
  ************************************************************************/
-
+`include "ysyx_22041211_define.v"
 module ysyx_22041211_pcPlus #(parameter DATA_LEN = 32)(
 	input		[DATA_LEN - 1:0]		pc_old,
 	input								reset,
@@ -12,7 +12,7 @@ module ysyx_22041211_pcPlus #(parameter DATA_LEN = 32)(
 );
 	always @(*) begin
 		if(reset)
-			pc_new = 32'h80000000;
+			pc_new = `PC_RESET_VAL;
 		else 
 			pc_new = pc_old + 32'b100;
 	
