0df5bc470c07ebc59460b4dafed21cacc1772ec4
tracer-ysyx <tracer@ysyx.org>
2024-10-10 15:49:08 +0800

    > sim RTL ysyx_22041211 王晓雪 Linux chelsea-virtual-machine 5.19.0-35-generic #36~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Feb 17 15:17:25 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:49:08 up  1:55,  2 users,  load average: 1.43, 1.64, 1.26

--- npc/csrc/cpu/difftest/dut.c
+++ npc/csrc/cpu/difftest/dut.c
@@ -203,9 +203,9 @@ void difftest_step(vaddr_t pc, vaddr_t npc) {
 // dut 0x8004 0x8008 0x800c 0x800f
   ref_difftest_exec(1);
   ref_difftest_regcpy(&ref_r, DIFFTEST_TO_DUT);
-  uint8_t ref_mem[4096] = {0};
-  ref_difftest_memcpy(0x8009df00, ref_mem, 256, DIFFTEST_TO_DUT);
-  check_mem(ref_mem, pc, 256,0x8009df00 );
+  // uint8_t ref_mem[4096] = {0};
+  // ref_difftest_memcpy(0x8009df00, ref_mem, 256, DIFFTEST_TO_DUT);
+  // check_mem(ref_mem, pc, 256,0x8009df00 );
   checkregs(&ref_r, npc);
 }
 #else
